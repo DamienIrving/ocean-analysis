1a6f9e6cfbdcf0336d41bff4c715cd3debd169c6
Damien Irving <irving.damien@gmail.com>
2017-04-05 10:55:48 +1000

    Functional

--- downloads/drstree_to_DRSv2.py
+++ downloads/drstree_to_DRSv2.py
@@ -9,12 +9,6 @@ import glob
 import pdb
 
 
-regular_file_list = glob.glob('/g/data/r87/dbi599/drstree/CMIP5/*/*/*/*/*/*i*p*/*.nc')
-dedrift_file_list = glob.glob('/g/data/r87/dbi599/drstree/CMIP5/*/*/*/*/*/*i*p*/*/*.nc')
-
-pdb.set_trace()
-
-
 def rename_files(file_list):
     """Rename a bunch of files."""
 
@@ -23,7 +17,7 @@ def rename_files(file_list):
 
         dir_components = infile.split('/')
         filename = dir_components.pop()
-        if 'dedrfited' in dir_components:
+        if 'dedrifted' in dir_components:
             dedrifted = True
             dir_components.pop() # remove dedrifted
         else:
@@ -31,39 +25,39 @@ def rename_files(file_list):
 
         if len(dir_components) == 13:
 
-			model = dir_components[7] 
-			experiment = dir_components[8]
-			timescale = dir_components[9]
-			realm = dir_components[10]
-			variable = dir_components[11]
-			run = dir_components[12]
+            model = dir_components[7] 
+            experiment = dir_components[8]
+            timescale = dir_components[9]
+            realm = dir_components[10]
+            variable = dir_components[11]
+            run = dir_components[12]
 	
-			if dedrifted:
-				new_dir = '/g/data/r87/dbi599/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/dedrifted/'  %(model, experiment, timescale, realm, run, variable)
-			else:
-				new_dir = '/g/data/r87/dbi599/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/'  %(model, experiment, timescale, realm, run, variable)
+            if dedrifted:
+                new_dir = '/g/data/r87/dbi599/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/dedrifted/'  %(model, experiment, timescale, realm, run, variable)
+            else:
+                new_dir = '/g/data/r87/dbi599/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/'  %(model, experiment, timescale, realm, run, variable)
 
-			mkdir_command = 'mkdir -p %s'  %(new_dir)
-			mv_command = 'mv %s %s'  %(infile, new_dir)    
-			old_dir = "/".join(dir_components)
-			rmdir_command = 'rmdir -r %s' %(old_dir)
+            mkdir_command = 'mkdir -p %s'  %(new_dir)
+            mv_command = 'mv %s %s'  %(infile, new_dir)    
+            old_dir = "/".join(dir_components)
+            rmdir_command = 'rmdir -r %s' %(old_dir)
 	  
-			print(mkdir_command)
-			os.system(mkdir_command)
+            print(mkdir_command)
+            os.system(mkdir_command)
 
-			print(mv_command)
-			os.system(mv_command)
-			
-			print(rmdir_command)  
-			os.system(rmdir_command)
+            print(mv_command)
+            os.system(mv_command)
 
         else:
         
             bogus_file_list.append("/".join(dir_components))
         
-    print bogus_file_list
+    print('bogus:', bogus_file_list)
     
 
+#dedrift_file_list = glob.glob('/g/data/r87/dbi599/drstree/CMIP5/*/*/*/*/*/*i*p*/*/*.nc')
 #rename_files(dedrift_file_list)
-#rename_files(regular_file_list)
+
+regular_file_list = glob.glob('/g/data/r87/dbi599/drstree/CMIP5/*/*/*/*/*/*i*p*/*.nc')
+rename_files(regular_file_list)
 
