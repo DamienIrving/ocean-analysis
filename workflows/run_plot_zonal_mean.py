983e67122a7e718556a14932cd076ff2b3fa95a0
Damien Irving <irving.damien@gmail.com>
2017-04-18 16:27:33 +1000

    Better handling of fx files

--- workflows/run_plot_zonal_mean.py
+++ workflows/run_plot_zonal_mean.py
@@ -16,24 +16,22 @@ import glob
 
 # Define functions
 
-def find_files(df, inargs, run, alt_experiment, experiment, tas=False):
+def find_files(df, run, alt_experiment, experiment, var, model, tscale, match=0, fx_physics=None):
     """Define the file names."""
     
-    if tas:
-        var = 'tas'
+    if var == 'tas':
         file_start = 'tas-global-mean'
         end = 'all.nc'
     else:
-        var = inargs.variable
         file_start = var
         end = '.nc'
 
-    df_selection = df.loc[(df['model'] == inargs.model) & (df['alt_experiment'] == alt_experiment)]
-    df_selection = df_selection[df_selection.run.str.contains(run+'i1')]
-    
+    df_selection = df.loc[(df['model'] == model) & (df['alt_experiment'] == alt_experiment)]
+    df_selection = df_selection[df_selection.run.str.contains(run+'i')]
+
     assert df_selection.shape[0] in [0, 1, 2]
     if df_selection.shape[0] == 2:
-        row_index = inargs.match
+        row_index = match
     else:
         row_index = 0
 
@@ -45,12 +43,12 @@ def find_files(df, inargs, run, alt_experiment, experiment, tas=False):
             run = df_selection['run'].values[row_index]
             if file_start == 'tas-global-mean':
                 data_dir = 'r87/dbi599'
-                tscale = 'yr'
                 realm = 'atmos'
             else:
-                tscale = 'mon'
                 realm = 'ocean'
-            file_list = glob.glob('/g/data/%s/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/%s_*%s' %(data_dir, inargs.model, experiment, tscale, realm, run, var, file_start, end))
+            if fx_physics:
+                run = 'r0i0'+fx_physics
+            file_list = glob.glob('/g/data/%s/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/%s_*%s' %(data_dir, model, experiment, tscale, realm, run, var, file_start, end))
             files = " ".join(file_list)
         else:
             files = ' '
@@ -62,7 +60,8 @@ def main(inargs, run):
     """Run the program."""
 
     command_list = ['python ../visualisation/plot_zonal_mean.py']
-    command_list.append('/g/data/r87/dbi599/figures/%s-zm/%s-zm_Oyr_%s_piControl-historical-GHG-AA_%s_198601-200512.png'  %(inargs.variable, inargs.variable, inargs.model, run))
+    aggregator = 'zi' if inargs.area else 'zm'
+    command_list.append('/g/data/r87/dbi599/figures/%s-%s/%s-%s_Oyr_%s_piControl-historical-GHG-AA_%s_198601-200512.png'  %(inargs.variable, aggregator, inargs.variable, aggregator, inargs.model, run))
     command_list.append(inargs.standard_name)
     command_list.append(inargs.model)
     command_list.append(run)
@@ -77,6 +76,7 @@ def main(inargs, run):
 
         exp_run = 'r1' if (experiment == 'piControl') else run
 
+        # Variable data files
         command_list.append('--' + alt_experiment.lower() + '_files')
         if inargs.variable == 'pe':
             if alt_experiment == 'historicalAA':
@@ -88,15 +88,41 @@ def main(inargs, run):
             file_list = glob.glob('/g/data/r87/dbi599/DRSv2/CMIP5/%s/%s/mon/atmos/%si1p%s/pe/latest/pe_*.nc' %(inargs.model, experiment, exp_run, str(physics)))
             files = " ".join(file_list)
         else:
-            files = find_files(df, inargs, exp_run, alt_experiment, experiment, tas=False)
+            files = find_files(df, exp_run, alt_experiment, experiment, inargs.variable, inargs.model, 'mon', match=inargs.match)
         command_list.append(files)
         
+        # Global mean temperature data files
         if alt_experiment != 'piControl':
             command_list.append('--' + alt_experiment.lower() + '_tas_file')
-            tas_file = find_files(df, inargs, exp_run, alt_experiment, experiment, tas=True)
+            tas_file = find_files(df, exp_run, alt_experiment, experiment, 'tas', inargs.model, 'yr', match=inargs.match)
             command_list.append(tas_file)        
 
-    command_list.append('--legloc ' + str(inargs.legloc))
+        # Cell area data files
+        command_list.append('--' + alt_experiment.lower() + '_area_file')
+        if alt_experiment == 'historicalAA' and inargs.model == 'CSIRO-Mk3-6-0':
+            fx_physics = 'p' + str(inargs.aa_physics)
+        elif alt_experiment == 'historicalnoAA' and inargs.model == 'CSIRO-Mk3-6-0':
+            fx_physics = 'p' + str(inargs.noaa_physics)
+        else:
+            fx_physics = 'p0'
+        area_file = find_files(df, exp_run, alt_experiment, experiment, 'areacello', inargs.model, 'fx', fx_physics=fx_physics)
+        command_list.append(area_file)        
+
+    if inargs.legloc:
+        legloc = inargs.legloc
+    elif inargs.variable == 'hfds':
+        legloc = 8
+    elif inargs.variable == 'pe':
+        legloc = 2
+    elif inargs.variable == 'tauuo':
+        legloc = 9
+    else:
+        legloc = 2 
+    command_list.append('--legloc ' + str(legloc))
+
+    if inargs.area:
+        command_list.append('--area_adjust ')
+
     final_command = " ".join(command_list)
     print(final_command)
     if inargs.execute:
@@ -129,11 +155,13 @@ author:
     parser.add_argument("--legloc", type=int, default=8,
                         help="Legend location")
     parser.add_argument("--aa_physics", type=int, default=None,
-                        help="Need to supply this for the P-E plot")
+                        help="Need to supply this for the P-E plot and CSIRO other plots")
     parser.add_argument("--noaa_physics", type=int, default=None,
-                        help="Need to supply this for the P-E plot")
+                        help="Need to supply this for the P-E plot and CSIRO other plots")
     parser.add_argument("--match", type=int, default=0,
                         help="Pick the first (index 0) or second (index 1) match - useful for GISS models with two p runs")
+    parser.add_argument("--area", action="store_true", default=False,
+                        help="Add the area adjustment flag")
 
     args = parser.parse_args()
     for run in args.runs:
