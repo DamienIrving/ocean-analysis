536258dce60b395899856f845328bc0384978136
Damien Irving <irving.damien@gmail.com>
2017-04-27 10:52:07 +1000

    Added option for creating symlinks

--- downloads/locate_nci_data.py
+++ downloads/locate_nci_data.py
@@ -27,20 +27,33 @@ def main(inargs):
                             model = inargs.model,
                             ensemble = inargs.ensemble)
  
-    path = '/g/data/ua6/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/*' %(inargs.model, inargs.experiment, inargs.time_freq, inargs.realm, inargs.ensemble, inargs.variable)
-    print('DRSv2:', glob.glob(path))
+    ua6_path = '/g/data/ua6/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest/*' %(inargs.model, inargs.experiment, inargs.time_freq, inargs.realm, inargs.ensemble, inargs.variable)
+    print('DRSv2:', glob.glob(ua6_path))
+
+    my_path = '/g/data/r87/dbi599/DRSv2/CMIP5/%s/%s/%s/%s/%s/%s/latest' %(inargs.model, inargs.experiment, inargs.time_freq, inargs.realm, inargs.ensemble, inargs.variable)
 
     print('Elsewhere path:')
+    elsewhere_path = []
     for o in outputs:
         var = o.variable
         for v in o.versions:
+            elsewhere_path.append(v.path)
             print(v.path)
-
+            
     print('Elsewhere files:')
     for f in outputs.first().filenames():
         six.print_(f)
-
-
+        if inargs.symlink:
+            assert len(elsewhere_path) == 1
+            command1 = 'mkdir -p %s' %(my_path)
+            command2 = 'ln -s %s/%s %s/%s'  %(elsewhere_path[0], f, my_path, f)
+            if inargs.execute:
+                os.system(command1)
+                os.system(command2)
+            else:
+                print(command1)
+                print(command2) 
+        
 if __name__ == '__main__':
 
     extra_info =""" 
@@ -66,6 +79,11 @@ dependencies:
     parser.add_argument("realm", type=str, help="e.g. atmos, ocean or aerosol")
     parser.add_argument("model", type=str, help="Model name")
     parser.add_argument("ensemble", type=str, help="e.g. r1i1p1")
+
+    parser.add_argument("--symlink", action="store_true", default=False,
+                        help="Create a symlink for the elsewhere files")
+    parser.add_argument("--execute", action="store_true", default=False,
+                        help="Execute the symlink command rather than printing to screen")
 
     args = parser.parse_args()             
     main(args)
