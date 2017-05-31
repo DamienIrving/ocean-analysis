57f3761cc6bc4baea1a4677c2b4cc3b132897aaa
Damien Irving <irving.damien@gmail.com>
2017-05-31 14:05:38 +1000

    Added history handling and area optional

--- data_processing/calc_surface_forcing_maps.py
+++ data_processing/calc_surface_forcing_maps.py
@@ -36,6 +36,8 @@ except ImportError:
 
 # Define functions
 
+history = []
+
 basins = {'atlantic': 2, 
           'pacific': 3,
           'indian': 5}
@@ -47,23 +49,27 @@ aggregation_abbreviations = {'mean': 'zm',
                              'sum': 'zs'}
 
 
-def get_history_attribute(data_file, data_cube, basin_file, basin_cube): # area_file, area_cube):
+def save_history(cube, field, filename):
+    """Save the history attribute when reading the data.
+    (This is required because the history attribute differs between input files 
+      and is therefore deleted upon equilising attributes)  
+    """ 
+
+    history.append(cube.attributes['history'])
+
+
+def get_history_attribute(data_file, data_history, basin_file, basin_cube):
     """Generate the history attribute for the output file."""
 
-    history_dict = {data_file: data_cube.attributes['history']}
+    history_dict = {data_file: data_history}
     if basin_file:
         history_dict[basin_file] = basin_cube.attributes['history']
-    #if area_file:
-    #    history_dict[area_file] = area_cube.attributes['history']
 
     return history_dict
         
 
 def multiply_by_area(cube):
     """Multiply by cell area."""
-
-    #area_cube = iris.load_cube(area_file)        
-    #cube = cube * area_cube
 
     if not cube.coord('latitude').has_bounds():
         cube.coord('latitude').guess_bounds()
@@ -81,7 +87,7 @@ def multiply_by_area(cube):
 def main(inargs):
     """Run the program."""
 
-    cube = iris.load(inargs.infiles, gio.check_iris_var(inargs.var))
+    cube = iris.load(inargs.infiles, gio.check_iris_var(inargs.var), callback=save_history)
 
     atts = cube[0].attributes
     equalise_attributes(cube)
@@ -91,7 +97,10 @@ def main(inargs):
 
     cube.attributes = atts
     orig_long_name = cube.long_name
-    orig_standard_name = cube.standard_name
+    if cube.standard_name == None:
+        orig_standard_name = orig_long_name.replace(' ', '_')
+    else:
+        orig_standard_name = cube.standard_name
     orig_var_name = cube.var_name
 
     # Temporal smoothing
@@ -108,17 +117,15 @@ def main(inargs):
     cube, coord_names, regrid_status = grids.curvilinear_to_rectilinear(cube)
 
     # Change units (remove m-2)
-    pdb.set_trace()
-    cube = multiply_by_area(cube)
-    cube.attributes = atts
-    cube.long_name = orig_long_name
-    cube.standard_name = orig_standard_name
-    cube.var_name = orig_var_name
+    if inargs.area:
+        cube = multiply_by_area(cube)
+        cube.attributes = atts
+        cube.long_name = orig_long_name
+        cube.standard_name = orig_standard_name
+        cube.var_name = orig_var_name
 
     # History
-    history_attribute = get_history_attribute(inargs.infiles[0], cube,
-                                              inargs.basin_file, basin_cube)
-                                              #inargs.area_file, area_cube)
+    history_attribute = get_history_attribute(inargs.infiles[0], history[0], inargs.basin_file, basin_cube)
     cube.attributes['history'] = gio.write_metadata(file_info=history_attribute)
 
     # Calculate output for each basin
@@ -176,11 +183,11 @@ note:
     
     parser.add_argument("--zonal_stat", type=str, choices=('mean', 'sum'), default='mean',
                         help="Zonal statistic")
+    parser.add_argument("--area", action="store_true", default=False,
+                        help="Multiply input data by area")
 
     parser.add_argument("--basin_file", type=str, default=None,
                         help="Cell basin file (for ocean input variables)")
-    #parser.add_argument("--area_file", type=str, default=None,
-    #                    help="Cell area file (used to remove m-2 from units)")
 
     args = parser.parse_args()            
     main(args)
