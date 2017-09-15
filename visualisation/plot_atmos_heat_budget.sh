43c079e0697dceb736e04e8ae2cd7af61b4c3893
Damien Irving <irving.damien@gmail.com>
2017-09-15 14:39:43 +1000

    Fixed path for fx data

--- visualisation/plot_atmos_heat_budget.sh
+++ visualisation/plot_atmos_heat_budget.sh
@@ -1,19 +1,21 @@
 # Script for running the plot_atmos_heat_budget.py script
 
-model=CSIRO-Mk3-6-0
-mip=r1i1p4
+model=FGOALS-g2
+mip=r2i1p1
 experiment=historicalMisc
 
-fx_mip=r0i0p4
-fx_experiment=${experiment}
+fx_mip=r0i0p0
+fx_experiment=historicalMisc
 
 python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
 script_dir=/home/599/dbi599/ocean-analysis/visualisation
 
 ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}/${experiment}
 r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}/${experiment}
+fx_ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}/${fx_experiment}
+fx_r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}/${fx_experiment}
 
-sftlf_file=${ua6_dir}/fx/atmos/${fx_mip}/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_${fx_mip}.nc
+sftlf_file=${fx_ua6_dir}/fx/atmos/${fx_mip}/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_${fx_mip}.nc
 
 outfile=/g/data/r87/dbi599/figures/heat-cycle/atmos-heat-budget_Oyr_${model}_${experiment}_${mip}_all.png   #_hf-atmos
 rsds_files="--rsds_files ${ua6_dir}/mon/atmos/${mip}/rsds/latest/rsds_Amon_${model}_${experiment}_${mip}_*.nc"
@@ -26,7 +28,7 @@ hfds_files="--hfds_files ${ua6_dir}/mon/ocean/${mip}/hfds/latest/hfds_Omon_${mod
 hfsithermds_files="--hfsithermds_files ${ua6_dir}/mon/ocean/${mip}/hfsithermds/latest/hfsithermds_Omon_${model}_${experiment}_${mip}_*.nc"
 
 command="${python} ${script_dir}/plot_atmos_heat_budget.py ${sftlf_file} ${outfile} ${rsds_files} ${rsus_files} ${rlds_files} ${rlus_files} ${hfss_files} ${hfls_files} ${hfds_files} --time 1850-01-01 2005-12-31"
-# ${hfsithermds_files} ${hfds_files} ${hfsithermds_files} --hfrealm ocean --plot_realm land 
+# ${hfsithermds_files} ${hfds_files} --hfrealm ocean --plot_realm land 
 
 echo ${command}
 ${command}
