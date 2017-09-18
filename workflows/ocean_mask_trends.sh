8eab932b0cb3ea43735de45ea7a60a361cb35978
Damien Irving <irving.damien@gmail.com>
2017-09-19 09:41:02 +1000

    Added atmos variable section

--- workflows/ocean_mask_trends.sh
+++ workflows/ocean_mask_trends.sh
@@ -1,11 +1,11 @@
 # Script for applying ocean mask then calculating trend
 
 model=CSIRO-Mk3-6-0
-mip=r1i1p1
-experiment=historical
+mip=r1i1p4
+experiment=historicalMisc
 
-fx_mip=r0i0p0
-fx_experiment=historical
+fx_mip=r0i0p4
+fx_experiment=historicalMisc
 
 python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
 script_dir=/home/599/dbi599/ocean-analysis
@@ -16,9 +16,9 @@ fx_ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}/${fx_experiment}
 fx_r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}/${fx_experiment}
 
 sftlf_file=${fx_ua6_dir}/fx/atmos/${fx_mip}/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_${fx_mip}.nc
-
-variables=(rsds rlus rlds hfss hfls)   
-for var in "${variables[@]}"; do
+  
+atmos_variables=(rsds rlus rlds hfss hfls pr evspsbl tas)   
+for var in "${atmos_variables[@]}"; do
 
     if [[ "${var}" == "rsds" ]] ; then
         standard_name=surface_downwelling_shortwave_flux_in_air
@@ -30,6 +30,12 @@ for var in "${variables[@]}"; do
         standard_name=surface_upward_sensible_heat_flux
     elif [[ "${var}" == "hfls" ]] ; then
         standard_name=surface_upward_latent_heat_flux
+    elif [[ "${var}" == "pr" ]] ; then
+        standard_name=precipitation_flux
+    elif [[ "${var}" == "evspsbl" ]] ; then
+        standard_name=water_evaporation_flux
+    elif [[ "${var}" == "tas" ]] ; then
+        standard_name=air_temperature
     fi
 
     mask_command="${python} ${script_dir}/downloads/apply_mask.py ${ua6_dir}/mon/atmos/${mip}/${var}/latest/${var}_Amon_${model}_${experiment}_${mip}_*.nc ${standard_name} ${sftlf_file} land_area_fraction sftlf --mask_label ocean"
@@ -43,15 +49,24 @@ for var in "${variables[@]}"; do
 
 done
 
-standard_name=surface_downward_heat_flux_in_sea_water
-outdir=${r87_dir}/mon/ocean/${mip}/hfds/latest
-outfile=${outdir}/hfds-trend_Omon_${model}_${experiment}_${mip}_1850-2005.nc
-mkdir -p ${outdir}
 
-trend_command="${python} ${script_dir}/data_processing/calc_trend.py ${ua6_dir}/mon/ocean/${mip}/hfds/latest/hfds_Omon_${model}_${experiment}_${mip}_*.nc ${standard_name} ${outfile} --time_bounds 1850-01-01 2005-12-31 --regrid"
-echo ${trend_command}
-${trend_command}
+ocean_variables=(tos hfds) 
+for var in "${ocean_variables[@]}"; do
 
- 
+    if [[ "${var}" == "tos" ]] ; then
+        standard_name=sea_surface_temperature
+    elif [[ "${var}" == "hfds" ]] ; then
+        standard_name=surface_downward_heat_flux_in_sea_water
+    fi
+
+    outdir=${r87_dir}/mon/ocean/${mip}/${var}/latest
+    outfile=${outdir}/${var}-trend_Omon_${model}_${experiment}_${mip}_1850-2005.nc
+    mkdir -p ${outdir}
+
+    trend_command="${python} ${script_dir}/data_processing/calc_trend.py ${ua6_dir}/mon/ocean/${mip}/${var}/latest/${var}_Omon_${model}_${experiment}_${mip}_*.nc ${standard_name} ${outfile} --time_bounds 1850-01-01 2005-12-31 --regrid"
+    echo ${trend_command}
+    ${trend_command}
+
+done 
 
 
