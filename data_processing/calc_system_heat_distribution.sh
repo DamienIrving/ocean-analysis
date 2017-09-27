9f1915f6df631f90c1e1ca22810da45904421c0d
Damien Irving <irving.damien@gmail.com>
2017-09-27 15:31:09 +1000

    Fixed bug

--- data_processing/calc_system_heat_distribution.sh
+++ data_processing/calc_system_heat_distribution.sh
@@ -9,26 +9,27 @@
 #
 # e.g. python plot_system_heat_distribution.py /g/data/r87/dbi599/DRSv2/CMIP5/CSIRO-Mk3-6-0/historicalMisc/yr/all/r1i1p4/energy-budget/latest/energy-budget_yr_CSIRO-Mk3-6-0_historicalMisc_r1i1p4_all.nc 
 #      /g/data/r87/dbi599/DRSv2/CMIP5/CSIRO-Mk3-6-0/historicalMisc/yr/all/r1i1p4/energy-budget/latest/energy-budget-trends_yr_CSIRO-Mk3-6-0_historicalMisc_r1i1p4_1850-2005.png 
-#      --aggregation trend --time 1850-01-01 2005-12-31
+#      --aggregation trend --time 1850-01-01 2005-12-31 --infer_ohc --infer_hfbasin  # --branch_time  29200 156
 
 model=CSIRO-Mk3-6-0
 mip=r1i1p1
 experiment=historicalGHG
+fx_experiment=historicalGHG
 
 python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
 script_dir=/home/599/dbi599/ocean-analysis/data_processing
 
-ua6_dir=/g/data/ua6/DRSv2_legacy/CMIP5/${model}/${experiment}
+ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}/${experiment}
 r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}/${experiment}
 
 outdir=${r87_dir}/yr/all/${mip}/energy-budget/latest
 outfile=${outdir}/energy-budget_yr_${model}_${experiment}_${mip}_all.nc
 
-sftlf_file=${ua6_dir}/fx/atmos/r0i0p0/sftlf/latest/sftlf_fx_${model}_${experiment}_r0i0p0.nc
+sftlf_file=${ua6_dir}/fx/atmos/r0i0p0/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_r0i0p0.nc
 
 rsdt_files="--rsdt_files ${ua6_dir}/mon/atmos/${mip}/rsdt/latest/rsdt_Amon_${model}_${experiment}_${mip}_*.nc"
 rsut_files="--rsut_files ${ua6_dir}/mon/atmos/${mip}/rsut/latest/rsut_Amon_${model}_${experiment}_${mip}_*.nc"
-rsut_files="--rlut_files ${ua6_dir}/mon/atmos/${mip}/rlut/latest/rlut_Amon_${model}_${experiment}_${mip}_*.nc"
+rlut_files="--rlut_files ${ua6_dir}/mon/atmos/${mip}/rlut/latest/rlut_Amon_${model}_${experiment}_${mip}_*.nc"
 rsds_files="--rsds_files ${ua6_dir}/mon/atmos/${mip}/rsds/latest/rsds_Amon_${model}_${experiment}_${mip}_*.nc"
 rsus_files="--rsus_files ${ua6_dir}/mon/atmos/${mip}/rsus/latest/rsus_Amon_${model}_${experiment}_${mip}_*.nc"
 rlds_files="--rlds_files ${ua6_dir}/mon/atmos/${mip}/rlds/latest/rlds_Amon_${model}_${experiment}_${mip}_*.nc"
@@ -38,12 +39,13 @@ hfss_files="--hfss_files ${ua6_dir}/mon/atmos/${mip}/hfss/latest/hfss_Amon_${mod
 hfls_files="--hfls_files ${ua6_dir}/mon/atmos/${mip}/hfls/latest/hfls_Amon_${model}_${experiment}_${mip}_*.nc"
 hfds_files="--hfds_files ${ua6_dir}/mon/ocean/${mip}/hfds/latest/hfds_Omon_${model}_${experiment}_${mip}_*.nc"
 hfbasin_files="--hfbasin_files ${ua6_dir}/mon/ocean/${mip}/hfbasin/latest/hfbasin_Omon_${model}_${experiment}_${mip}_*.nc"
+hfy_files="--hfy_files ${ua6_dir}/mon/ocean/${mip}/hfy/latest/hfy_Omon_${model}_${experiment}_${mip}_*.nc"
 
 ohc_files="--ohc_files ${r87_dir}/yr/ocean/${mip}/ohc/latest/ohc_Oyr_${model}_${experiment}_${mip}_*.nc"
 
 
 command="${python} ${script_dir}/calc_system_heat_distribution.py ${sftlf_file} ${outfile} ${rsdt_files} ${rsut_files} ${rlut_files} ${rsds_files} ${rsus_files} ${rlds_files} ${rlus_files} ${hfss_files} ${hfls_files} --hfrealm atmos ${hfds_files} ${hfbasin_files} ${ohc_files}"
-# 
+# ${hfbasin_files} ${hfy_files}
 
 echo ${command}
 ${command}
