bfff159ce9fb3374f2ecf13a6cc1dab9a11aea07
Damien Irving <irving.damien@gmail.com>
2017-10-16 16:20:13 +1100

    Added basins

--- visualisation/plot_wind_stress_metric.py
+++ visualisation/plot_wind_stress_metric.py
@@ -29,7 +29,7 @@ sys.path.append(modules_dir)
 try:
     import general_io as gio
     import timeseries
-    import spatial_weights
+    import convenient_universal as uconv
 except ImportError:
     raise ImportError('Must run this script from anywhere within the ocean-analysis git repo')
 
@@ -44,14 +44,27 @@ experiment_colors = {'historical': 'orange',
                      'rcp60': '#04471C',
                      'rcp85': '#0D2818'}
 
+basins = {'atlantic': 2, 
+          'pacific': 3,
+          'indian': 5,
+          'globe': 100}
 
-def load_data(infile):
+
+def load_data(infile, basin_cube, basin_name):
     """Load, temporally aggregate and spatially slice input data"""
-    
+
     cube = iris.load_cube(infile, 'surface_downward_x_stress')
     cube = timeseries.convert_to_annual(cube)
     experiment = cube.attributes['experiment_id']
     
+    if not basin_name == 'globe':
+        if basin_cube:
+            ndim = cube.ndim
+            basin_array = uconv.broadcast_array(basin_cube.data, [ndim - 2, ndim - 1], cube.shape) 
+        else:
+            basin_array = uconv.create_basin_array(cube)
+        cube.data.mask = numpy.where((cube.data.mask == False) & (basin_array == basins[basin_name]), False, True)        
+
     sh_constraint = iris.Constraint(latitude=lambda cell: -30.0 <= cell < 0.0)
     nh_constraint = iris.Constraint(latitude=lambda cell: 0.0 < cell <= 30.0)
     scrit_constraint = iris.Constraint(latitude=lambda cell: -17.0 <= cell < -13.0)
@@ -70,7 +83,7 @@ def load_data(infile):
     return sh_mean, nh_mean, scrit_mean, ncrit_mean, experiment
 
 
-def plot_data(ax, sh_cube, nh_cube, experiment, previous_experiments, crit=False):
+def plot_data(ax, sh_cube, nh_cube, experiment, previous_experiments, basin, crit=False):
     """Plot the various wind stress metrics."""
 
     plt.sca(ax)
@@ -84,9 +97,9 @@ def plot_data(ax, sh_cube, nh_cube, experiment, previous_experiments, crit=False
     plt.xlabel('Year')
 
     if crit:
-        plt.title('Critical latitude (15N/S)')
+        plt.title('Critical latitude (15N/S), %s' %(basin))
     else:
-        plt.title('Tropics (0-30N/S)')
+        plt.title('Tropics (0-30N/S), %s' %(basin))
 
     
 def main(inargs):
@@ -96,17 +109,29 @@ def main(inargs):
     time_constraints['historical'] = gio.get_time_constraint(inargs.hist_time)
     time_constraints['rcp'] = gio.get_time_constraint(inargs.rcp_time)
 
-    width=15
-    height=7
+    if inargs.basin_file:
+        basin_cube = iris.load_cube(inargs.basin_file)
+    else:
+        basin_cube = None
+
+    width=20
+    height=21
     fig = plt.figure(figsize=(width, height))
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax2 = fig.add_subplot(1, 2, 2)
- 
+    
+    ax_dict = {}
+    ax_dict[('tropics', 'atlantic')] = fig.add_subplot(3, 2, 1)
+    ax_dict[('critical', 'atlantic')] = fig.add_subplot(3, 2, 2)
+    ax_dict[('tropics', 'pacific')] = fig.add_subplot(3, 2, 3)
+    ax_dict[('critical', 'pacific')] = fig.add_subplot(3, 2, 4)
+    ax_dict[('tropics', 'globe')] = fig.add_subplot(3, 2, 5)
+    ax_dict[('critical', 'globe')] = fig.add_subplot(3, 2, 6)
+
     previous_experiments = []
     for filenum, infile in enumerate(inargs.infiles):
-        sh_mean, nh_mean, scrit_mean, ncrit_mean, experiment = load_data(infile)
-        plot_data(ax1, sh_mean, nh_mean, experiment, previous_experiments, crit=False)
-        plot_data(ax2, scrit_mean, ncrit_mean, experiment, previous_experiments, crit=True)
+        for basin in ['atlantic', 'pacific', 'globe']:
+            sh_mean, nh_mean, scrit_mean, ncrit_mean, experiment = load_data(infile, basin_cube, basin)
+            plot_data(ax_dict[('tropics', basin)], sh_mean, nh_mean, experiment, previous_experiments, basin, crit=False)
+            plot_data(ax_dict[('critical', basin)], scrit_mean, ncrit_mean, experiment, previous_experiments, basin, crit=True)
         previous_experiments.append(experiment)
 
     title = 'Annual Mean Surface Downward X Stress, %s'  %(sh_mean.attributes['model_id'])
@@ -135,6 +160,9 @@ author:
     parser.add_argument("infiles", type=str, nargs='*', 
                         help="Input wind stress files")                                                    
     parser.add_argument("outfile", type=str, help="Output file")  
+
+    parser.add_argument("--basin_file", type=str, default=None, 
+                        help="Input basin file")
 
     parser.add_argument("--hist_time", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'), default=None,
                         help="Time period [default = all]")
