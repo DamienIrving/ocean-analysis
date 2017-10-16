a774cff291a49daf4d1c10596f3953c7a1182c26
Damien Irving <irving.damien@gmail.com>
2017-10-16 11:21:21 +1100

    New spatial overturning script

---
+++ visualisation/plot_stc_spatial.py
@@ -0,0 +1,125 @@
+"""
+Filename:     plot_stc_spatial.py
+Author:       Damien Irving, irving.damien@gmail.com
+Description:  Plot metrics for the subtropical cells
+
+"""
+
+# Import general Python modules
+
+import sys, os, pdb
+import argparse
+import numpy
+import iris
+import iris.plot as iplt
+import matplotlib.pyplot as plt
+
+# Import my modules
+
+cwd = os.getcwd()
+repo_dir = '/'
+for directory in cwd.split('/')[1:]:
+    repo_dir = os.path.join(repo_dir, directory)
+    if directory == 'ocean-analysis':
+        break
+
+modules_dir = os.path.join(repo_dir, 'modules')
+sys.path.append(modules_dir)
+try:
+    import general_io as gio
+    import timeseries
+except ImportError:
+    raise ImportError('Must run this script from anywhere within the ocean-analysis git repo')
+
+
+# Define functions
+
+basin_index = {'pacific': 1,
+               'atlantic': 0,
+               'globe': 2}
+    
+
+def load_data(infile, basin):
+    """Load, temporally aggregate and spatially slice input data"""
+    
+    cube = iris.load_cube(infile, 'ocean_meridional_overturning_mass_streamfunction')
+    cube = cube[:, basin_index[basin], : ,:]
+    cube = timeseries.convert_to_annual(cube)
+    experiment = cube.attributes['experiment_id']
+    
+    depth_constraint = iris.Constraint(depth=lambda cell: cell <= 250)
+    lat_constraint = iris.Constraint(latitude=lambda cell: -30.0 <= cell < 30.0)
+
+    cube = cube.extract(depth_constraint & lat_constraint)
+    
+    return cube, experiment
+
+
+def plot_data(ax, cube, basin):
+    """Create the spatial plot."""
+
+    plt.sca(ax)
+
+    clim_cube = cube.collapsed('time', iris.analysis.MEAN)
+    if basin == 'atlantic':
+        levels = [-1.25e+10, -1.0e+10, -7.5e+9, -5.0e+9, -2.5e+9, 0, 2.5e+9, 5.0e+9, 7.5e+9, 1.0e+10, 1.25e+10]
+    else:
+        levels = [-5e+10, -4e+10, -3e+10, -2e+10, -1e+10, 0, 1e+10, 2e+10, 3e+10, 4e+10, 5e+10]
+    iplt.contourf(clim_cube, cmap='RdBu_r', levels=levels, extend='both')
+
+    cbar = plt.colorbar(orientation='horizontal')
+    cbar.set_label(str(clim_cube.units))
+
+    plt.ylabel('depth (%s)' %(cube.coord('depth').units))
+    plt.xlabel('latitude')    
+
+    plt.title(basin.title())
+
+    
+def main(inargs):
+    """Run the program."""
+
+    time_constraint = gio.get_time_constraint(inargs.time)
+
+    width=30
+    height=8
+    fig = plt.figure(figsize=(width, height))
+    ax_dict = {}
+    ax_dict['atlantic'] = fig.add_subplot(1, 3, 1)
+    ax_dict['pacific'] = fig.add_subplot(1, 3, 2)
+    ax_dict['globe'] = fig.add_subplot(1, 3, 3)
+    for basin in ['atlantic', 'pacific', 'globe']:
+        cube, experiment = load_data(inargs.infile, basin)
+        plot_data(ax_dict[basin], cube, basin)
+
+    title = 'Meridional Overturning Mass Streamfunction, Annual Climatology, %s (%s)'  %(cube.attributes['model_id'], experiment)
+    plt.suptitle(title, size='large')
+#    plt.subplots_adjust(top=0.90)
+
+    plt.savefig(inargs.outfile, bbox_inches='tight')
+    gio.write_metadata(inargs.outfile, file_info={inargs.infile: cube.attributes['history']})
+
+
+if __name__ == '__main__':
+
+    extra_info =""" 
+
+author:
+    Damien Irving, irving.damien@gmail.com
+
+"""
+
+    description = 'Plot a summary of the system heat distribution'
+    parser = argparse.ArgumentParser(description=description,
+                                     epilog=extra_info, 
+                                     argument_default=argparse.SUPPRESS,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+               
+    parser.add_argument("infile", type=str, help="Input msftmyz file")                                                    
+    parser.add_argument("outfile", type=str, help="Output file")  
+
+    parser.add_argument("--time", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'), default=None,
+                        help="Time period [default = all]")
+
+    args = parser.parse_args()             
+    main(args)
