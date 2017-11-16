bbf88c0caedd15a5ff34cef87c66901eeae0240b
Damien Irving <irving.damien@gmail.com>
2017-11-16 11:37:29 +1100

    Now works

--- data_processing/calc_wind_stress_metric.py
+++ data_processing/calc_wind_stress_metric.py
@@ -1,7 +1,7 @@
 """
 Filename:     calc_wind_stress_metric.py
 Author:       Damien Irving, irving.damien@gmail.com
-Description:  Calculate global metric
+Description:  Calculate zonal surface wind stress (over ocean) metrics
 
 """
 
@@ -31,7 +31,6 @@ try:
     import general_io as gio
     import convenient_universal as uconv
     import timeseries
-    import grids
 except ImportError:
     raise ImportError('Must run this script from anywhere within the ocean-analysis git repo')
 
@@ -46,6 +45,23 @@ def save_history(cube, field, filename):
     """ 
 
     history.append(cube.attributes['history'])
+
+
+def create_land_mask(sftlf_cube, target_shape):
+    """Create mask from an sftlf (land surface fraction) file.
+    
+    There is no land when cell value == 0
+    
+    """
+
+    target_ndim = len(target_shape)
+
+    mask_array = numpy.where(sftlf_cube.data < 50, False, True)
+
+    mask = uconv.broadcast_array(mask_array, [target_ndim - 2, target_ndim - 1], target_shape)
+    assert mask.shape == target_shape 
+
+    return mask
 
 
 def wind_stress_metrics(xdata, ydata, hemisphere, direction):
@@ -75,40 +91,49 @@ def wind_stress_metrics(xdata, ydata, hemisphere, direction):
 
 
 def create_outcubes(metric_dict, atts, units, time_coord):
-   """Create an iris cube for each metric."""
+    """Create an iris cube for each metric."""
    
     cube_list = []
-    for metadata, data in metric_dict.iteritems():
-        standard_name = standard_base+'_'+region
-        long_name = '%s %s'  %(long_base, region.replace('_', ' '))
-        var_name = metric_name+'_'+region
+    for metadata, data in metric_dict.items():
+        hemisphere, direction, metric = metadata
+        standard_name = '%s_%s_%s' %(hemisphere, direction, metric)
+        long_name = '%s %s %s' %(hemisphere, direction, metric)
+        var_name = '%s_%s_%s' %(hemisphere, direction, metric[0:3])
         iris.std_names.STD_NAMES[standard_name] = {'canonical_units': units}
-        metric_cube = iris.cube.Cube(metric_dict[region].data,
+        metric_cube = iris.cube.Cube(data,
                                      standard_name=standard_name,
                                      long_name=long_name,
                                      var_name=var_name,
                                      units=units,
                                      attributes=atts,
                                      dim_coords_and_dims=[(time_coord, 0)],
-                                  )
-        cube_list.append(ohc_cube)        
+                                     )
+        cube_list.append(metric_cube)        
 
     cube_list = iris.cube.CubeList(cube_list)
     cube_list = cube_list.concatenate()
 
     return cube_list
 
+
 def main(inargs):
     """Run the program."""
 
     # Read data
-    cube = iris.load(inargs.infiles, gio.check_iris_var(inargs.var), callback=save_history)
+    cube = iris.load(inargs.infiles, 'surface_downward_eastward_stress', callback=save_history)
     equalise_attributes(cube)
     iris.util.unify_time_units(cube)
     cube = cube.concatenate_cube()
     cube = gio.check_time_units(cube)
 
+    # Prepare data
     cube = timeseries.convert_to_annual(cube)
+
+    sftlf_cube = iris.load_cube(inargs.sftlf_file, 'land_area_fraction')
+    mask = create_land_mask(sftlf_cube, cube.shape)
+    cube.data = numpy.ma.asarray(cube.data)
+    cube.data.mask = mask
+    
     cube = cube.collapsed('longitude', iris.analysis.MEAN)
 
     # Calculate metrics
@@ -122,9 +147,9 @@ def main(inargs):
     for hemisphere, direction in itertools.product(hemispheres, directions):
         metric_dict[(hemisphere, direction, 'location')] = []
         metric_dict[(hemisphere, direction, 'magnitude')] = []
-    
-    for ydata in cube.slices(['latitude']):
-        func = interp1d(xdata, ydata, kind='cubic')
+
+    for ycube in cube.slices(['latitude']):
+        func = interp1d(xdata, ycube.data, kind='cubic')
         ynew = func(xnew)
         for hemisphere, direction in itertools.product(hemispheres, directions):
             loc, mag = wind_stress_metrics(xnew, ynew, hemisphere, direction)
@@ -132,9 +157,12 @@ def main(inargs):
             metric_dict[(hemisphere, direction, 'magnitude')].append(mag)
             
     # Write the output file
-    cube_list = create_outcubes(metric_dict)
+    atts = cube.attributes
+    infile_history = {inargs.infiles[0]: history[0]}
+    atts['history'] = gio.write_metadata(file_info=infile_history)
+    cube_list = create_outcubes(metric_dict, cube.attributes, cube.units, cube.coord('time'))
 
-    iris.save(metric, inargs.outfile)
+    iris.save(cube_list, inargs.outfile)
 
 
 if __name__ == '__main__':
@@ -145,15 +173,14 @@ author:
 
 """
 
-    description='Calculate a global metric'
+    description='Calculate zonal surface wind stress (over ocean) metrics'
     parser = argparse.ArgumentParser(description=description,
                                      epilog=extra_info, 
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument("infiles", type=str, nargs='*', help="Input data files (can merge on time)")
-    parser.add_argument("var", type=str, help="Input variable name (i.e. the standard_name)")
-    parser.add_argument("metric", type=str, choices=('mean', 'bulk-deviation', 'grid-deviation'), help="Metric to calculate")
+    parser.add_argument("infiles", type=str, nargs='*', help="Wind stress (tauu) data files (can merge on time)")
+    parser.add_argument("sftlf_file", type=str, help="Land area fraction file for ")
     parser.add_argument("outfile", type=str, help="Output file name")
     
     args = parser.parse_args()            
