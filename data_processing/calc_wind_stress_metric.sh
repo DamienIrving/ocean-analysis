c172327ce31c5b5775dfd644f2b7d46832ac0013
Damien Irving <irving.damien@gmail.com>
2017-11-17 16:35:32 +1100

    Fixed typo with rip

--- data_processing/calc_wind_stress_metric.sh
+++ data_processing/calc_wind_stress_metric.sh
@@ -2,11 +2,11 @@
 
 # Script for running the calc_wind_stress_metric.py script
 
-model=CSIRO-Mk3-6-0
-mip=r1i1p4
-experiment=historicalMisc
+model=CCSM4
+rip=r4i1p1
+experiment=historicalGHG
 
-fx_mip=r0i0p0
+fx_rip=r0i0p0
 fx_experiment=historical
 
 python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
@@ -17,10 +17,10 @@ r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}/${experiment}
 fx_ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}/${fx_experiment}
 fx_r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}/${fx_experiment}
 
-sftlf_file=${fx_ua6_dir}/fx/atmos/${fx_mip}/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_${fx_mip}.nc
-outdir=${r87_dir}/yr/atmos/r1i1p1/tauu/latest
-outfile=${outdir}/tauu-metrics_Ayr_${model}_${experiment}_${mip}_all.nc
-tauu_files="${ua6_dir}/mon/atmos/${mip}/tauu/latest/tauu_Amon_${model}_${experiment}_${mip}_*.nc"
+sftlf_file=${fx_ua6_dir}/fx/atmos/${fx_rip}/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_${fx_rip}.nc
+outdir=${r87_dir}/yr/atmos/${rip}/tauu/latest
+outfile=${outdir}/tauu-metrics_Ayr_${model}_${experiment}_${rip}_all.nc
+tauu_files="${ua6_dir}/mon/atmos/${rip}/tauu/latest/tauu_Amon_${model}_${experiment}_${rip}_*.nc"
 
 command="${python} ${script_dir}/calc_wind_stress_metric.py ${tauu_files} ${sftlf_file} ${outfile}" 
 mkdir -p ${outdir}
