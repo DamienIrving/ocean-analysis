d930044727e3ac9bfd852e730f312457e8837cab
Damien Irving <irving.damien@gmail.com>
2017-12-05 17:05:04 +1100

    Fixed typo in mkdir

--- data_processing/calc_zonal_aggregate.sh
+++ data_processing/calc_zonal_aggregate.sh
@@ -1,7 +1,8 @@
-model=GFDL-CM3
 
-experiment=historicalGHG
-rips=(r5i1p1)
+model=GISS-E2-R
+
+experiment=historical
+rips=(r2i1p1)
 
 fx_rip=r0i0p0
 fx_experiment=historical
@@ -13,17 +14,19 @@ ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}
 r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}
 
 
-for var in evspsbl; do
+for var in pe; do
 
 if [[ "${var}" == 'pr' ]] ; then
     standard_name='precipitation_flux'
 elif [[ "${var}" == 'evspsbl' ]] ; then
     standard_name='water_evaporation_flux'
+elif [[ "${var}" == 'pe' ]] ; then
+    standard_name='precipitation_minus_evaporation_flux'
 fi
 
 for rip in "${rips[@]}"; do
 
-mkdir -p /g/data/r87/dbi599/DRSv2/CMIP5/${model}/${experiment}/yr/ocean/${rip}/${var}/latest/
+mkdir -p /g/data/r87/dbi599/DRSv2/CMIP5/${model}/${experiment}/yr/atmos/${rip}/${var}/latest/
 
 input_file=${r87_dir}/${experiment}/mon/atmos/${rip}/${var}/latest/${var}-ocean_Amon_${model}_${experiment}_${rip}_*.nc
 
