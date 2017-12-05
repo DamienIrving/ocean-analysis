e8aba646409bf09b5bba2b5cc9cf4b98d1d76ca6
Damien Irving <irving.damien@gmail.com>
2017-12-05 17:04:27 +1100

    Added P-E

--- downloads/apply_mask.sh
+++ downloads/apply_mask.sh
@@ -1,17 +1,17 @@
 
 
 experiment=historicalMisc
-fx_experiment=historicalMisc
-model=CSIRO-Mk3-6-0
-mip=r1i1p3
-datadir=ua6
+fx_experiment=historical
+model=GFDL-CM3
+rips=(r1i1p1 r3i1p1 r5i1p1)
+datadir=r87/dbi599
 fxdir=ua6
-fxmip=r0i0p3
+fxrip=r0i0p0
 
 # rsds rsus rlds rlus hfss hfls
-# pr evspsbl
+# pr evspsbl pe
 
-for var in evspsbl pr; do
+for var in pe; do
 
 if [[ "${var}" == "rsds" ]] ; then
     standard_name='surface_downwelling_shortwave_flux_in_air'
@@ -29,8 +29,17 @@ elif [[ "${var}" == 'pr' ]] ; then
     standard_name='precipitation_flux'
 elif [[ "${var}" == 'evspsbl' ]] ; then
     standard_name='water_evaporation_flux'
+elif [[ "${var}" == 'pe' ]] ; then
+    standard_name='precipitation_minus_evaporation_flux'
 fi
 
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python apply_mask.py /g/data/${datadir}/DRSv2/CMIP5/${model}/${experiment}/mon/atmos/${mip}/${var}/latest/${var}_Amon_${model}_${experiment}_${mip}_*.nc ${standard_name} /g/data/${fxdir}/DRSv2/CMIP5/${model}/${fx_experiment}/fx/atmos/${fxmip}/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_${fxmip}.nc land_area_fraction sftlf --ocean
+for rip in "${rips[@]}"; do
+
+command="/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python apply_mask.py /g/data/${datadir}/DRSv2/CMIP5/${model}/${experiment}/mon/atmos/${rip}/${var}/latest/${var}_Amon_${model}_${experiment}_${rip}_*.nc ${standard_name} /g/data/${fxdir}/DRSv2/CMIP5/${model}/${fx_experiment}/fx/atmos/${fxrip}/sftlf/latest/sftlf_fx_${model}_${fx_experiment}_${fxrip}.nc land_area_fraction sftlf --mask_label ocean"
+
+echo ${command}
+${command}
+
+done
 
 done
