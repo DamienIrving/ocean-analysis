4a9029fc174daea7ed671c0cbb8c1d2b1a3161eb
Damien Irving <irving.damien@gmail.com>
2018-02-27 12:03:57 +1100

    Ready for use

--- data_processing/calc_heat_budget_change.py
+++ data_processing/calc_heat_budget_change.py
@@ -8,10 +8,13 @@ Description:  Calculate the heat budget change between two time periods
 # Import general Python modules
 
 import sys, os, pdb
+import collections
 import argparse
 import numpy
+import pandas
 import iris
 import iris.coord_categorisation
+
 
 # Import my modules
 
@@ -47,37 +50,13 @@ def save_history(cube, field, filename):
     history.append(cube.attributes['history'])
 
 
-def convert_to_joules(cube):
-    """Convert units to Joules"""
-
-    assert 'W' in str(cube.units)
-    assert 'days' in str(cube.coord('time').units)
-    
-    time_span_days = cube.coord('time').bounds[:, 1] - cube.coord('time').bounds[:, 0]
-    time_span_seconds = time_span_days * 60 * 60 * 24
-    
-    cube.data = cube.data * uconv.broadcast_array(time_span_seconds, 0, cube.shape)
-    cube.units = str(cube.units).replace('W', 'J')
-    
-    return cube
-    
-
-def get_data(infile):
-     """Get the data."""
-
-    data_dict = {}
-    cube_list = iris.load(infile)
-
-    cube_list = iris.load(infile)
-
-
-def get_control_time_constraints(control_cube, hist_cube):
-    """ """
-
-    branch_time = hist_cube.attributes['branch_time']
+def get_control_time_constraint(control_cube, hist_cube, time_bounds):
+    """Define the time constraints for the control data."""
 
     iris.coord_categorisation.add_year(control_cube, 'time')
     iris.coord_categorisation.add_year(hist_cube, 'time')
+
+    branch_time = hist_cube.attributes['branch_time']
     
     index = 0
     for bounds in control_cube.coord('time').bounds:
@@ -89,24 +68,84 @@ def get_control_time_constraints(control_cube, hist_cube):
 
     branch_year = control_cube.coord('year').points[index]
     hist_start_year = hist_cube.coord('year').points[0]
+    start_gap = int(time_bounds[0].split('-')[0]) - hist_start_year
+    end_gap = int(time_bounds[1].split('-')[0]) - hist_start_year
 
-    time_constraint = get_time_constraint([str(81).zfill(4)+'-01-01', str(100).zfill(4)+'-12-31'])
+    control_start_year = branch_year + start_gap
+    control_end_year = branch_year + end_gap
 
-    ## To do = account for the fact that the selected historical time period might not start at 1850.
+    control_start_date = str(control_start_year).zfill(4)+'-01-01'
+    control_end_date = str(control_end_year).zfill(4)+'-01-01'
+
+    time_constraint = get_time_constraint([control_start_date, control_end_date])
+
+    return time_constraint
+
+
+def get_attributes(cube):
+    """Get the model, experiment and rip information."""
+
+    model = cube.attributes['model_id']
+    experiment = cube.attributes['experiment_id']
+ 
+    realization = cube.attributes['realization']
+    initialization = cube.attributes['initialization_method']
+    physics = cube.attributes['physics_version']
+    
+    rip = 'r%si%sp%s' %(realization, initialization, physics)
+
+    return model, experiment, rip
+
+
+def generate_results(data_dict, cube_list, time_constraint, time_bounds):
+    """Generate results."""
+
+    model, experiment, rip = get_attributes(cube_list[0])
+    period = '%s-%s' %(time_bounds[0].split('-')[0], time_bounds[0].split('-')[1])
+
+    data_dict['model'].append(model)
+    data_dict['experiment'].append(experiment)
+    data_dict['rip'].append(rip)
+    data_dict['period'].append(period)
+
+    for cube in cube_list:
+        temporal_subset = cube.extract(time_constraint)
+        clim = temporal_subset.collapsed('time', iris.analysis.MEAN)
+        data_dict[clim.var_name] = clim.data
+
+    return data_dict
 
         
 def main(inargs):
     """Run the program."""
  
+    hist_cube_list = iris.load(inargs.historical_file)
+    control_cube_list = iris.load(inargs.control_file)
+
     hist_start_constraint = gio.get_time_constraint(inargs.start_time)
     hist_end_constraint = gio.get_time_constraint(inargs.end_time)
-
-    hist_cube_list = iris.load(inargs.historical_file)
-   
-    control_cube_list = iris.load(inargs.control_file)
-    control_start_constraint, control_end_constraint = get_control_time_constraints(control_cube_list[0], hist_cube_list[0])
+    start_year = inargs.start_time[0].split('-')[0]
+    control_start_constraint = get_control_time_constraint(control_cube_list[0], hist_cube_list[0], inargs.start_time)
+    control_end_constraint = get_control_time_constraint(control_cube_list[0], hist_cube_list[0], inargs.end_time)
     
+    column_headers = ['model', 'experiment', 'rip', 'period',
+                      'hfds-globe-sum', 'hfds-nh-sum', 'hfds-sh-sum', 'hfds-nhext-sum', 'hfds-tropics-sum', 'hfds-shext-sum',
+                      'ohc-globe-sum', 'ohc-nh-sum', 'ohc-sh-sum', 'ohc-nhext-sum', 'ohc-tropics-sum', 'ohc-shext-sum']
 
+    data_dict = collections.OrderedDict()
+    for column in column_headers:
+        data_dict[column] = []
+    data_dict = generate_results(data_dict, hist_cube_list, hist_start_constaint, inargs.start_time)
+    data_dict = generate_results(data_dict, hist_cube_list, hist_end_constaint, inargs.end_time)
+    data_dict = generate_results(data_dict, control_cube_list, control_start_constaint, inargs.start_time)
+    data_dict = generate_results(data_dict, control_cube_list, control_end_constaint, inargs.end_time)
+
+    data_df = pandas.DataFrame.from_dict(data_dict)
+    data_df.to_csv(inargs.outfile)
+
+    metadata_dict = {inargs.historical_file: hist_cube_list[0].attributes['history'],
+                     inargs.control_file: control_cube_list[0].attributes['history']}
+    gio.write_metadata(inargs.outfile, file_info=metadata_dict)
 
 
 if __name__ == '__main__':
