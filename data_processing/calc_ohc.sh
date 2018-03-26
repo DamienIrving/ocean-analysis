d438c110f3f9ee9f631ea5b6be93bc058fc2345f
Damien Irving <irving.damien@gmail.com>
2018-03-26 16:49:11 +1100

    Fixed typo with fx variables

--- data_processing/calc_ohc.sh
+++ data_processing/calc_ohc.sh
@@ -1,6 +1,6 @@
 
 
-model=FGOALS-g2
+model=MPI-ESM-MR
 
 experiment=piControl
 rip=r1i1p1
@@ -15,9 +15,9 @@ ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}
 r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}
 
 
-command="${python} ${script_dir}/calc_ohc.py ${ua6_dir}/${experiment}/mon/ocean/${rip}/thetao/latest/thetao_Omon_${model}_${experiment}_${rip}_*.nc sea_water_potential_temperature --annual --area_file ${ua6_dir}/historical/fx/ocean/r0i0p0/areacello/latest/areacello_fx_${model}_historical_r0i0p0.nc --chunk"
+command="${python} ${script_dir}/calc_ohc.py ${ua6_dir}/${experiment}/mon/ocean/${rip}/thetao/latest/thetao_Omon_${model}_${experiment}_${rip}_*.nc sea_water_potential_temperature --annual --area_file ${ua6_dir}/${fx_experiment}/fx/ocean/${fx_rip}/areacello/latest/areacello_fx_${model}_${fx_experiment}_${fx_rip}.nc"
 
-# --area_file ${ua6_dir}/historical/fx/ocean/r0i0p0/areacello/latest/areacello_fx_${model}_historical_r0i0p0.nc --volume_file ${ua6_dir}/historical/fx/ocean/r0i0p0/volcello/latest/volcello_fx_${model}_historical_r0i0p0.nc --chunk
+# --area_file ${ua6_dir}/${fx_experiment}/fx/ocean/${fx_rip}/areacello/latest/areacello_fx_${model}_${fx_experiment}_${fx_rip}.nc --volume_file ${ua6_dir}/${fx_experiment}/fx/ocean/${fx_rip}/volcello/latest/volcello_fx_${model}_${fx_experiment}_${fx_rip}.nc --chunk
 #
 
 echo ${command}
