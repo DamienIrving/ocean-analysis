82b571ce7941d438f7931ff497079d08f242a308
Damien Irving <irving.damien@gmail.com>
2018-05-09 15:07:29 +1000

    Added rndt

--- data_processing/calc_interhemispheric_metric.sh
+++ data_processing/calc_interhemispheric_metric.sh
@@ -6,11 +6,11 @@ rips=(r1i1p4)
 agg='sum'
 # mean sum
 
-metric='global-fraction'
+metric='diff'
 # diff global-fraction
 
-var='ohc'
-# hfds ohc
+var='rndt'
+# hfds ohc rndt
 
 inferred=false
 # true false
@@ -32,26 +32,40 @@ if [[ "${var}" == "ohc" ]] ; then
     longvar=ocean_heat_content
     areafile=${ua6_dir}/${fx_experiment}/fx/ocean/${fx_rip}/areacello/latest/areacello_fx_${model}_${fx_experiment}_${fx_rip}.nc
     outvar=ohc
+    outtscale='Oyr'
+    outrealm='ocean'
     smooth=' '
 elif [ "${var}" == "hfds" ] && [ "${inferred}" == true ] ; then
     infiles=${r87_dir}/${experiment}/mon/ocean/${rip}/hfds/latest/hfds-inferred_Omon_${model}_${experiment}_${rip}_*.nc
     longvar=surface_downward_heat_flux_in_sea_water
     areafile=${ua6_dir}/${fx_experiment}/fx/atmos/${fx_rip}/areacella/latest/areacella_fx_${model}_${fx_experiment}_${fx_rip}.nc
     outvar=hfds-inferred
+    outtscale='Oyr'
+    outrealm='ocean'
     smooth='--annual'
 elif [[ "${var}" == 'hfds' ]] ; then
     infiles=${ua6_dir}/${experiment}/mon/ocean/${rip}/hfds/latest/hfds_Omon_${model}_${experiment}_${rip}_*.nc
     longvar=surface_downward_heat_flux_in_sea_water
     areafile=${ua6_dir}/${fx_experiment}/fx/ocean/${fx_rip}/areacello/latest/areacello_fx_${model}_${fx_experiment}_${fx_rip}.nc
     outvar=hfds
+    outtscale='Oyr'
+    outrealm='ocean'
+    smooth='--annual'
+elif [[ "${var}" == "rndt" ]] ; then
+    infiles=${r87_dir}/${experiment}/mon/atmos/${rip}/rndt/latest/rndt_Amon_${model}_${experiment}_${rip}_*.nc
+    longvar='TOA_Incoming_Net_Radiation'
+    areafile=${ua6_dir}/${fx_experiment}/fx/atmos/${fx_rip}/areacella/latest/areacella_fx_${model}_${fx_experiment}_${fx_rip}.nc
+    outvar=rndt
+    outtscale='Ayr'
+    outrealm='atmos'
     smooth='--annual'
 fi
-outdir=${r87_dir}/${experiment}/yr/ocean/${rip}/${var}/latest
+outdir=${r87_dir}/${experiment}/yr/${outrealm}/${rip}/${var}/latest
 mkdir -p ${outdir}
-outfile=${outdir}/${outvar}-${agg}-hemispheric-metrics_Oyr_${model}_${experiment}_${rip}_all.nc
+outfile=${outdir}/${outvar}-${agg}-hemispheric-metrics_${outtscale}_${model}_${experiment}_${rip}_all.nc
 
 
-command="${python} ${script_dir}/calc_interhemispheric_metric.py ${infiles} ${longvar} ${outfile} --metric ${metric} --aggregation_method ${agg} ${smooth}"
+command="${python} ${script_dir}/calc_interhemispheric_metric.py ${infiles} ${longvar} ${outfile} --metric ${metric} --aggregation_method ${agg} ${smooth} --area_file ${areafile}"
 # --annual --nh_lat_bounds -3.5 91 --sh_lat_bounds -91 -3.5 --chunk --area_file ${areafile}
 
 echo ${command}
