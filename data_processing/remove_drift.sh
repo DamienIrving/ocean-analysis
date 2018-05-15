eb7de42b8ca4cbf32b89c50fd93ec5e7bdc07b4e
Damien Irving <irving.damien@gmail.com>
2018-05-15 16:33:57 +1000

    Added more variables

--- data_processing/remove_drift.sh
+++ data_processing/remove_drift.sh
@@ -1,27 +1,27 @@
 
-model=CanESM2
+model=FGOALS-g2
 
 experiments=(historicalMisc)
-rip=r1i1p4
+rip=r2i1p1
 
 control_rip=r1i1p1
 
-indtype=rndt-sum-hemispheric-metrics
+indtype=hfds-zonal-sum
 # ohc-zonal-sum ohc-sum-hemispheric-metrics hfbasin-global hfds-inferred-sum-hemispheric-metrics hfds-sum-hemispheric-metrics rndt-sum-hemispheric-metrics
 
-var=rndt
+var=hfds
 # ohc hfbasin hfds rndt
 
-var_long=TOA_Incoming_Net_Radiation_globe_sum
-# ocean_heat_content ocean_heat_content_globe_sum ocean_heat_content_nh_sum_div_globe_sum northward_ocean_heat_transport Downward_Heat_Flux_at_Sea_Water_Surface_globe_sum TOA_Incoming_Net_Radiation_globe_sum
+var_long=surface_downward_heat_flux_in_sea_water
+# ocean_heat_content ocean_heat_content_globe_sum ocean_heat_content_nh_sum_div_globe_sum northward_ocean_heat_transport surface_downward_heat_flux_in_sea_water Downward_Heat_Flux_at_Sea_Water_Surface_globe_sum TOA_Incoming_Net_Radiation_globe_sum TOA_Incoming_Net_Radiation_nh_sum TOA_Incoming_Net_Radiation_sh_sum
 
-outdtype=rndt-globe-sum
-# ohc-zonal-sum ohc-nh-sum-div-globe-sum hfbasin-global hfds-inferred-globe-sum hfds-globe-sum rndt-globe-sum
+outdtype=hfds-zonal-sum
+# ohc-zonal-sum ohc-nh-sum-div-globe-sum hfbasin-global hfds-inferred-globe-sum hfds-globe-sum rndt-globe-sum rndt-nh-sum rndt-sh-sum
 
-realm=atmos
+realm=ocean
 #atmos ocean
 
-tscale=Ayr
+tscale=Oyr
 #Ayr Oyr
 
 python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
@@ -32,16 +32,16 @@ r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}
 
 for experiment in "${experiments[@]}"; do
 
-control_file=${r87_dir}/piControl/yr/${realm}/${control_rip}/${var}/latest/${indtype}_${tscale}_${model}_piControl_${control_rip}_cumsum-all.nc
-coefficient_file=${r87_dir}/piControl/yr/${realm}/${control_rip}/${var}/latest/${outdtype}-coefficients_${tscale}_${model}_piControl_${control_rip}_cumsum-all.nc
+control_file=${r87_dir}/piControl/yr/${realm}/${control_rip}/${var}/latest/${indtype}_${tscale}_${model}_piControl_${control_rip}_all.nc
+coefficient_file=${r87_dir}/piControl/yr/${realm}/${control_rip}/${var}/latest/${outdtype}-coefficients_${tscale}_${model}_piControl_${control_rip}_all.nc
 experiment_dir=${r87_dir}/${experiment}/yr/${realm}/${rip}/${var}/latest
-experiment_file=${experiment_dir}/${indtype}_${tscale}_${model}_${experiment}_${rip}_cumsum-all.nc
+experiment_file=${experiment_dir}/${indtype}_${tscale}_${model}_${experiment}_${rip}_all.nc
 dedrifted_dir=${experiment_dir}/dedrifted
-dedrifted_file=${dedrifted_dir}/${outdtype}_${tscale}_${model}_${experiment}_${rip}_cumsum-all.nc
+dedrifted_file=${dedrifted_dir}/${outdtype}_${tscale}_${model}_${experiment}_${rip}_all.nc
 
 coefficient_command="${python} ${script_dir}/calc_drift_coefficients.py ${control_file} ${var_long} ${coefficient_file}"
 mkdir_command="mkdir ${dedrifted_dir}"
-drift_command="${python} ${script_dir}/remove_drift.py ${experiment_file} ${var_long} annual ${coefficient_file} ${dedrifted_file}"
+drift_command="${python} ${script_dir}/remove_drift.py ${experiment_file} ${var_long} annual ${coefficient_file} ${dedrifted_file} --branch_time 175382.5"
 # --branch_time 342005 (CCSM4) 175382.5 (FGOALS-g2) 0 (GISS-E2-R) --no_parent_check
 
 echo ${coefficient_command}
