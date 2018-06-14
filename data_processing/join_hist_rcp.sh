ba45232eb2ad33afeaa42a3a9d1d8f857febf6b3
Damien Irving <irving.damien@gmail.com>
2018-06-14 11:36:03 +1000

    Now functional

--- data_processing/join_hist_rcp.sh
+++ data_processing/join_hist_rcp.sh
@@ -3,8 +3,10 @@ model=CanESM2
 experiment=rcp85
 mip=r1i1p1
 
-var=TOA_Incoming_Net_Radiation
-# TOA_Incoming_Net_Radiation ocean_heat_content
+var=ohc
+# rndt hfds ohc
+
+inferred=false
 
 fx_rip=r0i0p0
 fx_experiment=historical
@@ -12,11 +14,39 @@ fx_experiment=historical
 python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
 script_dir=/home/599/dbi599/ocean-analysis/data_processing
 
-ua6_dir=/g/data/ua6/DRSv2/CMIP5/${model}
 r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}
 
+if [[ "${var}" == "rndt" ]] ; then
+    long_name=TOA_Incoming_Net_Radiation
+    file_ext='cumsum-all.nc'
+    tscale='Ayr'
+    realm='atmos'
+    cumsum='--cumsum'
+elif [[ "${var}" == "hfds" ]] ; then
+    long_name=Downward_Heat_Flux_at_Sea_Water_Surface
+    file_ext='cumsum-all.nc'
+    tscale='Oyr'
+    realm='ocean'
+    cumsum='--cumsum'
+elif [[ "${var}" == "ohc" ]] ; then
+    long_name=ocean_heat_content
+    file_ext='all.nc'
+    tscale='Oyr'
+    realm='ocean'
+    cumsum=' '
+fi
 
-command="${python} ${script_dir}/join_hist_rcp.py ${ua6_dir}/historical/yr/atmos/r1i1p1/rndt/latest/rndt-sum-hemispheric-metrics_Ayr_CanESM2_historical_r1i1p1_cumsum-all.nc ${ua6_dir}/rcp85/yr/atmos/r1i1p1/rndt/latest/rndt-sum-hemispheric-metrics_Ayr_CanESM2_rcp85_r1i1p1_cumsum-all.nc ${ua6_dir}/rcp85/yr/atmos/r1i1p1/rndt/latest/rndt-sum-hemispheric-metrics_Ayr_CanESM2_historical-rcp85_r1i1p1_cumsum-all.nc TOA_Incoming_Net_Radiation_globe_sum TOA_Incoming_Net_Radiation_nh_sum TOA_Incoming_Net_Radiation_sh_sum --cumsum"
+if [ "${inferred}" == true ] ; then
+    file_label=${var}-inferred-sum-hemispheric-metrics
+else
+    file_label=${var}-sum-hemispheric-metrics
+fi
+
+hist_file=${r87_dir}/historical/yr/${realm}/r1i1p1/${var}/latest/${file_label}_${tscale}_${model}_historical_r1i1p1_${file_ext}
+rcp_file=${r87_dir}/rcp85/yr/${realm}/r1i1p1/${var}/latest/${file_label}_${tscale}_${model}_rcp85_r1i1p1_${file_ext}
+outfile=${r87_dir}/rcp85/yr/${realm}/r1i1p1/${var}/latest/${file_label}_${tscale}_${model}_historical-rcp85_r1i1p1_${file_ext}
+
+command="${python} ${script_dir}/join_hist_rcp.py ${hist_file} ${rcp_file} ${outfile} ${long_name}_globe_sum ${long_name}_nh_sum ${long_name}_sh_sum ${cumsum}"
 
 echo ${command}
 ${command}
