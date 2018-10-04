ac0935cca4b064f2da46bb07cf02889a2cbfb8e1
Damien Irving <irving.damien@gmail.com>
2018-10-04 16:24:03 +1000

    Now functional

--- visualisation/plot_hemispheric_heat_barchart.py
+++ visualisation/plot_hemispheric_heat_barchart.py
@@ -8,12 +8,14 @@ Description:  Plot barchart comparing heat uptake/storage in either hemisphere
 # Import general Python modules
 
 import sys, os, pdb
+import re
 import argparse
-import iris
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib.ticker as tkr
 import seaborn
-
+import pandas
+import iris
 import cmdline_provenance as cmdprov
 
 # Import my modules
@@ -35,29 +37,34 @@ except ImportError:
 
 # Define functions
 
-mpl.rcParams['axes.labelsize'] = 24
-mpl.rcParams['axes.titlesize'] = 28
-mpl.rcParams['xtick.labelsize'] = 24
-mpl.rcParams['ytick.labelsize'] = 24
-mpl.rcParams['legend.fontsize'] = 24
+mpl.rcParams['axes.labelsize'] = 16
+mpl.rcParams['axes.titlesize'] = 20
+mpl.rcParams['xtick.labelsize'] = 16
+mpl.rcParams['ytick.labelsize'] = 16
+mpl.rcParams['legend.fontsize'] = 16
 
 
-def calc_hemispheric_heat(nh_file, sh_file, var, time_constraint):
+def calc_hemispheric_heat(sh_file, nh_file, var, time_constraint):
     """Calculate the interhemispheric difference timeseries."""
+
+    names = {'ohc': 'ocean heat content',
+             'hfds': 'Downward Heat Flux at Sea Water Surface',
+             'rndt': 'TOA Incoming Net Radiation',
+             'thetao': 'Sea Water Potential Temperature'}
+
+    sh_name = names[var] + ' sh sum'
+    sh_cube = iris.load_cube(sh_file, sh_name & time_constraint)
+    sh_value = sh_cube.data[-1] - sh_cube.data[0]
 
     nh_name = names[var] + ' nh sum'
     nh_cube = iris.load_cube(nh_file, nh_name & time_constraint)
     nh_value = nh_cube.data[-1] - nh_cube.data[0]
- 
-    sh_name = names[var] + ' sh sum'
-    sh_cube = iris.load_cube(sh_file, sh_name & time_constraint)
-    sh_value = sh_cube.data[-1] - sh_cube.data[0]
     
     experiment = nh_cube.attributes['experiment_id']
     model = nh_cube.attributes['model_id']
     history = nh_cube.attributes['history']
     
-    return sh_value, nh_value, experiment, history
+    return sh_value, nh_value, experiment, model, history
 
 
 def generate_heat_row_dicts(column_list, sh_value, nh_value, model, experiment, var, ylabel):
@@ -83,12 +90,17 @@ def generate_heat_row_dicts(column_list, sh_value, nh_value, model, experiment, 
 def get_ylabel(toa_files, ohu_files, ohc_files):
     """Get the label for the yaxis"""
     
-    if (toa_files or ohu_files) and ohc_files:
-        ylabel = 'heat uptake/storage (J)'
-    elif not ohc_files:
+    toa_plot = False if toa_files == [] else True
+    ohu_plot = False if ohu_files == [] else True
+    ohc_plot = False if ohc_files == [] else True
+
+    ylabel = 'heat uptake/storage (J)'
+    if not ohc_plot:
         ylabel = 'heat uptake (J)'
-    else:
+    elif not (toa_plot or ohu_plot):
         ylabel = 'heat storage (J)'
+
+    return ylabel
 
 
 def main(inargs):
@@ -104,14 +116,15 @@ def main(inargs):
         for model_files in var_files:
             for file_pair in [model_files[i:i + 2] for i in range(0, len(model_files), 2)]:
                 sh_file, nh_file = file_pair  
-                sh_value, nh_value, experiment, model, history = calc_hemispheric_heat(nh_file, sh_file, var, time_constraint)
+                sh_value, nh_value, experiment, model, history = calc_hemispheric_heat(sh_file, nh_file, var, time_constraint)
                 column_list = generate_heat_row_dicts(column_list, sh_value, nh_value, model, experiment, var, ylabel)
     heat_df = pandas.DataFrame(column_list)
 
-    g = sns.catplot(x="hemisphere", y=ylabel,
-                   hue="experiment", col="variable",
-                   data=hemisphere_df, kind="bar",
-                   palette=['0.5', 'red', 'blue'])
+    seaborn.set_style("whitegrid")
+    g = seaborn.catplot(x="hemisphere", y=ylabel,
+                        hue="experiment", col="variable",
+                        data=heat_df, kind="bar",
+                        palette=['0.5', 'red', 'blue'])
     g.set_titles("{col_name}")
 
     dpi = inargs.dpi if inargs.dpi else plt.savefig.__globals__['rcParams']['figure.dpi']
@@ -141,11 +154,14 @@ author:
     parser.add_argument("outfile", type=str, help="output file")
 
     parser.add_argument("--toa_files", type=str, nargs=6, action='append', default=[],
-                        help="netTOA files in this order: hist NH, hist SH, GHG NH, GHG SH, AA NH, AA SH")                     
+                        help="netTOA files in this order: hist SH, hist NH, GHG SH, GHG NH, AA SH, AA NH")                     
     parser.add_argument("--ohu_files", type=str, nargs=6, action='append', default=[],
-                        help="OHU files in this order: hist NH, hist SH, GHG NH, GHG SH, AA NH, AA SH")
+                        help="OHU files in this order: hist SH, hist NH, GHG SH, GHG NH, AA SH, AA NH")
     parser.add_argument("--ohc_files", type=str, nargs=6, action='append', default=[],
-                        help="OHC files in this order: hist NH, hist SH, GHG NH, GHG SH, AA NH, AA SH")
+                        help="OHC files in this order: hist SH, hist NH, GHG SH, GHG NH, AA SH, AA NH")
+
+    parser.add_argument("--dpi", type=float, default=None,
+                        help="Figure resolution in dots per square inch [default=auto]")
 
     parser.add_argument("--time", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'),
                         default=('1861-01-01', '2005-12-31'),
