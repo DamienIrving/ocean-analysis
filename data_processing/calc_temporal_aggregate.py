396628be597ba2fb14932ab9e46a9bd19bcb332a
Damien Irving <irving.damien@gmail.com>
2018-12-04 16:00:42 +1100

    Processing single or multi-file

--- data_processing/calc_temporal_aggregate.py
+++ data_processing/calc_temporal_aggregate.py
@@ -1,18 +1,19 @@
 """
 Filename:     calc_temporal_aggregate.py
 Author:       Damien Irving, irving.damien@gmail.com
-Description:  Calculate the linear trend or climatology
+Description:  Calculate the temporal aggregate
 
 """
 
 # Import general Python modules
 
-import sys, os, pdb
+import sys, os, pdb, re
 import argparse
 import numpy, math
 import iris
 from iris.experimental.equalise_cubes import equalise_attributes
 from decimal import Decimal
+import cmdline_provenance as cmdprov
 
 # Import my modules
 
@@ -31,7 +32,6 @@ try:
     import timeseries
     import grids
     import convenient_universal as uconv
-    import spatial_weights
 except ImportError:
     raise ImportError('Must run this script from anywhere within the ocean-analysis git repo')
 
@@ -76,74 +76,28 @@ def get_agg_cube(cube, agg):
     return agg_cube
 
 
-def lat_tropics(cell):
-    return -30 < cell < 30
+def get_constraints(inargs):
+    """Get the time, depth and mask information"""
+
+    time_constraint = gio.get_time_constraint(inargs.time_bounds)
+    depth_constraint = gio.iris_vertical_constraint(0.0, inargs.max_depth)
+
+    if inargs.land_mask:
+        sftlf_cube = iris.load_cube(inargs.land_mask, 'land_area_fraction')
+    else:
+        sftlf_cube = None
+
+    return time_constraint, depth_constraint, sftlf_cube
 
 
-def subtract_tropics(cube):
-    """Subtract the mean tropics trend."""
-
-    lat_constraint = iris.Constraint(latitude=lat_tropics)
-    tropics_cube = cube.extract(lat_constraint)
-    tropics_mean = tropics_cube.collapsed(['longitude', 'latitude'], iris.analysis.MEAN)
-
-    cube.data = cube.data - tropics_mean.data
-
-    return cube
-
-
-def vertical_mean(cube):
-    """Calculate the vertical mean"""
-
-    coord_names = [coord.name() for coord in cube.dim_coords]
-    depth_axis = cube.coord('depth')
-
-    assert depth_axis.units in ['m', 'dbar'], "Unrecognised depth axis units"
-    if depth_axis.units == 'm':
-        weights = spatial_weights.calc_vertical_weights_1D(depth_axis, coord_names, cube.shape)
-    elif depth_axis.units == 'dbar':
-        assert coord_names == ['time', 'depth', 'latitude', 'longitude'], "2D weights will not work for curvilinear grid"
-        weights = spatial_weights.calc_vertical_weights_2D(depth_axis, cube.coord('latitude'), coord_names, cube.shape)
-
-    cube = cube.collapsed('depth', iris.analysis.MEAN, weights=weights)
-    cube.remove_coord('depth')
-
-    return cube
-
-
-def main(inargs):
-    """Run the program."""
-
-    # Read data
-    try:
-        time_constraint = gio.get_time_constraint(inargs.time_bounds)
-    except AttributeError:
-        time_constraint = iris.Constraint()
-
-    depth_constraint = gio.iris_vertical_constraint(inargs.min_depth, inargs.max_depth)
-
-    cube = iris.load(inargs.infiles, gio.check_iris_var(inargs.var) & depth_constraint)
-    history = cube[0].attributes['history']
-    atts = cube[0].attributes
-    equalise_attributes(cube)
-    iris.util.unify_time_units(cube)
-    cube = cube.concatenate_cube()
-
-    cube = gio.check_time_units(cube)
-    cube = cube.extract(time_constraint)
-
-    cube = iris.util.squeeze(cube)
+def process_cube(cube, inargs, sftlf_cube):
+    """Process a data cube"""
 
     if 'salinity' in inargs.var:
         cube = gio.salinity_unit_check(cube)
 
-    infile_metadata = {inargs.infiles[0]: history}
-
     if inargs.annual:
         cube = timeseries.convert_to_annual(cube, full_months=True)
-
-    if inargs.min_depth or inargs.max_depth:
-        cube = vertical_mean(cube)
 
     if inargs.aggregation:
         cube = get_agg_cube(cube, inargs.aggregation)
@@ -158,19 +112,74 @@ def main(inargs):
             print('After sum:', '%.2E' % Decimal(cube.data.sum()) )
             print('Before mean:', '%.2E' % Decimal(before_mean) )
             print('After mean:', '%.2E' % Decimal(cube.data.mean()) )
-
-    if inargs.subtract_tropics:
-        cube = subtract_tropics(cube)             
-
-    if inargs.land_mask:
-        sftlf_cube = iris.load_cube(inargs.land_mask, 'land_area_fraction')
+            
+    if sftlf_cube:
         cube = uconv.apply_land_ocean_mask(cube, sftlf_cube, 'ocean')
 
-    atts['history'] = gio.write_metadata(file_info=infile_metadata)
-    cube.attributes = atts
+    return cube
 
-    iris.FUTURE.netcdf_no_unlimited = True
-    iris.save(cube, inargs.outfile)
+
+def new_file_name(filename, inargs):
+    """Take the original file name and create new output file name."""
+
+    infile = filename.split('/')[-1]
+
+    if inargs.annual:
+        infile = re.sub('Omon', 'Oyr', infile)
+        infile = re.sub('Amon', 'Ayr', infile)
+
+    if inargs.max_depth:
+        depth_text = '_0-%sm' %(str(int(inargs.max_depth)))
+        infile = re.sub('.nc', depth_text + '.nc', infile)
+
+    outfile = inargs.outfile + infile
+
+    print('output:', outfile)
+
+    return outfile
+
+
+def combine_infiles(inargs, time_constraint, depth_constraint):
+    """Combine multiple input files into one cube"""
+
+    cube = iris.load(inargs.infiles, gio.check_iris_var(inargs.var) & depth_constraint)
+    history = cube[0].attributes['history']
+    atts = cube[0].attributes
+    equalise_attributes(cube)
+    iris.util.unify_time_units(cube)
+    cube = cube.concatenate_cube()
+
+    cube = gio.check_time_units(cube)
+    cube = cube.extract(time_constraint)
+    cube = iris.util.squeeze(cube)
+
+    log = cmdprov.new_log(infile_history={inargs.infiles[0]: atts['history']}, git_repo=repo_dir)
+    cube.attributes['history'] = log
+
+    return cube
+
+
+def main(inargs):
+    """Run the program."""
+
+    time_constraint, depth_constraint, sftlf_cube = get_constraints(inargs)
+    nfiles = len(inargs.infiles)
+    if inargs.aggregation or nfiles == 1:
+        assert inargs.outfile[-3:] == '.nc'
+        cube = combine_infiles(inargs, time_constraint, depth_constraint)
+        cube = process_cube(cube, inargs, sftlf_cube)
+        iris.save(cube, inargs.outfile)
+    else:
+        assert inargs.outfile[-1] == '/'
+        for fnum, filename in enumerate(inargs.infiles):
+            cube = iris.load_cube(filename, gio.check_iris_var(inargs.var) & depth_constraint)
+            cube = gio.check_time_units(cube)
+            cube = process_cube(cube, inargs, sftlf_cube)
+                   
+            outfile = new_file_name(filename, inargs)
+            log = cmdprov.new_log(infile_history={filename: cube.attributes['history']}, git_repo=repo_dir) 
+            cube.attributes['history'] = log
+            iris.save(cube, outfile)
 
 
 if __name__ == '__main__':
@@ -181,7 +190,7 @@ author:
     
 """
 
-    description='Calculate the temporal aggregate (linear trend or climatology) at each grid point'
+    description='Calculate the temporal aggregate at each grid point'
     parser = argparse.ArgumentParser(description=description,
                                      epilog=extra_info, 
                                      argument_default=argparse.SUPPRESS,
@@ -189,29 +198,25 @@ author:
 
     parser.add_argument("infiles", type=str, nargs='*', help="Input files")
     parser.add_argument("var", type=str, help="Variable standard_name")
-    parser.add_argument("outfile", type=str, help="Output file name")
+    parser.add_argument("outfile", type=str, help="Output file name (or directory if you want each of many input files processed separately")
 
     parser.add_argument("--aggregation", type=str, choices=('trend', 'clim'), default=None,
                         help="Method for temporal aggregation [default = None]")
 
-    parser.add_argument("--time_bounds", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'),
+    parser.add_argument("--annual", action="store_true", default=False,
+                        help="Convert data to annual mean [default=False]")
+
+    parser.add_argument("--time_bounds", type=str, nargs=2, default=None, metavar=('START_DATE', 'END_DATE'),
                         help="Time period [default = entire]")
 
-    parser.add_argument("--min_depth", type=float, default=None,
-                        help="Only include data below this vertical level")
     parser.add_argument("--max_depth", type=float, default=None,
                         help="Only include data above this vertical level")
 
-    parser.add_argument("--subtract_tropics", action="store_true", default=False,
-                        help="Subtract the mean tropics trend from all data points")
     parser.add_argument("--regrid", action="store_true", default=False,
                         help="Regrid to a regular lat/lon grid")
 
     parser.add_argument("--land_mask", type=str, default=None,
                         help="Name of sftlf file to use for land mask")
-
-    parser.add_argument("--annual", action="store_true", default=False,
-                        help="Convert data to annual mean before calculating aggregate [default=False]")
 
     args = parser.parse_args()            
     main(args)
