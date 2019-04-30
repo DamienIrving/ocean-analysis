22a6bf6b2e1a76740c2f1c42be1841bf6c7056c0
Damien Irving <irving.damien@gmail.com>
2019-04-30 14:43:42 +1000

    Much better now

---
+++ data_processing/calc_temporal_aggregate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Description: Run calc_temporal_aggregate.py over a bunch of files
+#             
+
+function usage {
+    echo "USAGE: bash $0 infiles"
+    echo "   infiles:      Input file names"
+    exit 1
+}
+
+python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
+script_dir=/home/599/dbi599/ocean-analysis/data_processing
+
+infiles=($@)
+
+for infile in "${infiles[@]}"; do
+ 
+    outfile=`echo ${infile} | sed s:ua6:r87/dbi599:`
+    outfile=`echo ${outfile} | sed s:DRSv3:DRSv2:`
+    outfile=`echo ${outfile} | sed s:Omon:Oyr:`
+    outfile=`echo ${outfile} | sed s:/mon/:/yr/:`
+    command="${python} ${script_dir}/calc_temporal_aggregate.py ${infile} sea_water_potential_temperature ${outfile} --annual"
+
+    echo ${command}
+    ${command}
+    echo ${outfile}
+
+done
