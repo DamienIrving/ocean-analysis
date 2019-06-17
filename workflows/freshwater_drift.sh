a4bad210300fcac9e4e2d643ba2bc992d74f61ce
Damien Irving <irving.damien@gmail.com>
2019-06-17 16:03:18 +1000

    Moisture zonal integral workflow

--- workflows/freshwater_drift.sh
+++ workflows/freshwater_drift.sh
@@ -1,5 +1,5 @@
-model=CSIRO-Mk3-6-0
-experiment=historicalGHG
+model=IPSL-CM5A-LR
+experiment=historical
 #rip=r1i1p1
 
 python=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
@@ -15,13 +15,13 @@ mkdir -p ${r87_dir}/piControl/yr/ocean/r1i1p1/wfo/latest/
 
 wfo_control_metric="${python} ${script_dir}/calc_interhemispheric_metric.py ${ua6_dir}/piControl/mon/ocean/r1i1p1/wfo/latest/wfo_Omon_${model}_piControl_r1i1p1_??????-??????.nc water_flux_into_sea_water ${r87_dir}/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-sum-hemispheric-metrics_Oyr_${model}_piControl_r1i1p1_cumsum-all.nc --weights_file ${ua6_dir}/historical/fx/ocean/r0i0p0/areacello/latest/areacello_fx_${model}_historical_r0i0p0.nc --aggregation_method sum --annual --cumsum --flux_to_mag"
 
-#echo ${wfo_control_metric}
-#${wfo_control_metric}
+echo ${wfo_control_metric}
+${wfo_control_metric}
 
 wfo_coefficients="${python} ${script_dir}/calc_drift_coefficients.py ${r87_dir}/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-sum-hemispheric-metrics_Oyr_${model}_piControl_r1i1p1_cumsum-all.nc Water_Flux_into_Sea_Water_globe_sum ${r87_dir}/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-globe-sum-coefficients_Oyr_${model}_piControl_r1i1p1_cumsum-all.nc"
 
-#echo ${wfo_coefficients}
-#${wfo_coefficients}
+echo ${wfo_coefficients}
+${wfo_coefficients}
 
 mkdir -p ${r87_dir}/${experiment}/yr/ocean/r1i1p1/wfo/latest/dedrifted
 
@@ -42,13 +42,13 @@ mkdir -p ${r87_dir}/piControl/yr/ocean/r1i1p1/soga/latest/
 
 soga_control_metric="${python} ${script_dir}/calc_temporal_aggregate.py ${ua6_dir}/piControl/mon/ocean/r1i1p1/soga/latest/soga_Omon_${model}_piControl_r1i1p1_??????-??????.nc sea_water_salinity ${r87_dir}/piControl/yr/ocean/r1i1p1/soga/latest/soga_Oyr_${model}_piControl_r1i1p1_all.nc --annual"
 
-#echo ${soga_control_metric}
-#${soga_control_metric}
+echo ${soga_control_metric}
+${soga_control_metric}
 
 soga_coefficients="${python} ${script_dir}/calc_drift_coefficients.py ${r87_dir}/piControl/yr/ocean/r1i1p1/soga/latest/soga_Oyr_${model}_piControl_r1i1p1_all.nc sea_water_salinity ${r87_dir}/piControl/yr/ocean/r1i1p1/soga/latest/soga-coefficients_Oyr_${model}_piControl_r1i1p1_all.nc"
 
-#echo ${soga_coefficients}
-#${soga_coefficients}
+echo ${soga_coefficients}
+${soga_coefficients}
 
 
 mkdir -p ${r87_dir}/${experiment}/yr/ocean/r1i1p1/soga/latest/dedrifted
--- workflows/zonal_water_budget.sh
+++ workflows/zonal_water_budget.sh
@@ -1,58 +1,87 @@
-def calc_anomaly(cube):
-    """Calculate the anomaly."""
-    
-    anomaly = cube.copy()
-    anomaly.data = anomaly.data - anomaly.data[0]
-    anomaly = anomaly[-1, ::]
-    anomaly.remove_coord('time')
-    
-    return anomaly
 
+model=NorESM1-M
+experiment=historical
+#rip=r1i1p1
 
+python="/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python -W ignore"
+script_dir=/home/599/dbi599/ocean-analysis/data_processing
 
-cube = iris.load_cube(infile, var & time_constraint)
+ua6_dir=/g/data/ua6/DRSv3/CMIP5/${model}
+r87_dir=/g/data/r87/dbi599/DRSv2/CMIP5/${model}
 
-anomaly = calc_anomaly(cube)
+area_file=${ua6_dir}/${experiment}/fx/ocean/r0i0p0/areacello/latest/areacello_fx_${model}_${experiment}_r0i0p0.nc
+ref_file="--ref_file ${ua6_dir}/${experiment}/mon/atmos/r1i1p1/rsdt/latest/rsdt_Amon_${model}_${experiment}_r1i1p1_185001-200512.nc toa_incoming_shortwave_flux"
+# " " if already lat/lon
 
-ocean_convergence = ohc_anomaly - hfds_anomaly
-hfbasin_inferred = ocean_convergence.copy()
-hfbasin_inferred.data = numpy.ma.cumsum(-1 * ocean_convergence.data)
-
-
-iplt.plot(rndt_anomaly, color='red', label=labels[0],
-linestyle=linestyle, linewidth=linewidth)
-
-
-plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0), useMathText=True)
-ax.yaxis.major.formatter._useMathText = True
 
 
 ## wfo
 
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/remove_drift.py /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/historical/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum_Oyr_NorESM1-M_historical_r1i1p1_cumsum-all.nc water_flux_into_sea_water annual /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum-coefficients_Oyr_NorESM1-M_piControl_r1i1p1_cumsum-all.nc /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/historical/yr/ocean/r1i1p1/wfo/latest/dedrifted/wfo-zonal-sum_Oyr_NorESM1-M_historical_r1i1p1_cumsum-all.nc --no_data_check
+### Control zonal sum
+control_dir=${r87_dir}/piControl/yr/ocean/r1i1p1/wfo/latest
+mkdir -p ${control_dir}
 
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/calc_drift_coefficients.py /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum_Oyr_NorESM1-M_piControl_r1i1p1_cumsum-all.nc  water_flux_into_sea_water /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum-coefficients_Oyr_NorESM1-M_piControl_r1i1p1_cumsum-all.nc
+control_zonal_sum_file=${control_dir}/wfo-zonal-sum_Oyr_${model}_piControl_r1i1p1_cumsum-all.nc
+control_zonal_sum_command="${python} ${script_dir}/calc_zonal_aggregate.py ${ua6_dir}/piControl/mon/ocean/r1i1p1/wfo/latest/wfo_Omon_${model}_piControl_r1i1p1_??????-??????.nc water_flux_into_sea_water sum ${control_zonal_sum_file} --annual ${ref_file} --cumsum --area ${area_file} --flux_to_mag"
 
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/calc_zonal_aggregate.py /g/data/ua6/DRSv3/CMIP5/NorESM1-M/historical/mon/ocean/r1i1p1/wfo/latest/wfo_Omon_NorESM1-M_historical_r1i1p1_??????-??????.nc water_flux_into_sea_water sum /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/historical/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum_Oyr_NorESM1-M_historical_r1i1p1_cumsum-all.nc --annual --ref_file /g/data/ua6/DRSv2/CMIP5/NorESM1-M/historical/mon/atmos/r1i1p1/rsdt/latest/rsdt_Amon_NorESM1-M_historical_r1i1p1_185001-200512.nc --cumsum --area /g/data/ua6/DRSv3/CMIP5/NorESM1-M/historical/fx/ocean/r0i0p0/areacello/latest/areacello_fx_NorESM1-M_historical_r0i0p0.nc --flux_to_mag
+#echo ${control_zonal_sum_command}
+# ${control_zonal_sum_command}
 
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/calc_zonal_aggregate.py /g/data/ua6/DRSv3/CMIP5/NorESM1-M/piControl/mon/ocean/r1i1p1/wfo/latest/wfo_Omon_NorESM1-M_piControl_r1i1p1_??????-??????.nc water_flux_into_sea_water sum /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum_Oyr_NorESM1-M_piControl_r1i1p1_cumsum-all.nc --annual --ref_file /g/data/ua6/DRSv2/CMIP5/NorESM1-M/historical/mon/atmos/r1i1p1/rsdt/latest/rsdt_Amon_NorESM1-M_historical_r1i1p1_185001-200512.nc toa_incoming_shortwave_flux --cumsum --area /g/data/ua6/DRSv3/CMIP5/NorESM1-M/historical/fx/ocean/r0i0p0/areacello/latest/areacello_fx_NorESM1-M_historical_r0i0p0.nc --flux_to_mag
+### Experiment zonal sum
+experiment_zonal_sum_file=${r87_dir}/${experiment}/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum_Oyr_${model}_${experiment}_r1i1p1_cumsum-all.nc
+experiment_zonal_sum_command="${python} ${script_dir}/calc_zonal_aggregate.py ${ua6_dir}/${experiment}/mon/ocean/r1i1p1/wfo/latest/wfo_Omon_${model}_${experiment}_r1i1p1_??????-??????.nc water_flux_into_sea_water sum ${experiment_zonal_sum_file} --annual ${ref_file} --cumsum --area ${area_file} --flux_to_mag"
+
+#echo ${experiment_zonal_sum_command}
+#${experiment_zonal_sum_command}
+
+### Drift coefficients
+wfo_coefficient_file=${r87_dir}/piControl/yr/ocean/r1i1p1/wfo/latest/wfo-zonal-sum-coefficients_Oyr_${model}_piControl_r1i1p1_cumsum-all.nc
+wfo_coefficient_command="${python} ${script_dir}/calc_drift_coefficients.py ${control_zonal_sum_file} water_flux_into_sea_water ${wfo_coefficient_file}"
+
+#echo ${wfo_coefficient_command}
+#${wfo_coefficient_command}
+
+### Remove drift
+wfo_drift_command="${python} ${script_dir}/remove_drift.py ${experiment_zonal_sum_file} water_flux_into_sea_water annual ${wfo_coefficient_file} ${r87_dir}/${experiment}/yr/ocean/r1i1p1/wfo/latest/dedrifted/wfo-zonal-sum_Oyr_${model}_${experiment}_r1i1p1_cumsum-all.nc --no_data_check"
+
+#echo ${wfo_drift_command}
+#${wfo_drift_command}
 
 
 ## so
 
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/remove_drift.py /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/historical/yr/ocean/r1i1p1/so/latest/so-vertical-zonal-mean_Oyr_NorESM1-M_historical_r1i1p1_all.nc sea_water_salinity annual  /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/so/latest/so-vertical-zonal-mean-coefficients_Oyr_NorESM1-M_piControl_r1i1p1_all.nc 
-/g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/historical/yr/ocean/r1i1p1/so/latest/dedrifted/so-vertical-zonal-mean_Oyr_NorESM1-M_historical_r1i1p1_all.nc --no_data_check
-
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/calc_drift_coefficients.py /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/so/latest/so-vertical-zonal-mean_Oyr_NorESM1-M_piControl_r1i1p1_all.nc sea_water_salinity /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/so/latest/so-vertical-zonal-mean-coefficients_Oyr_NorESM1-M_piControl_r1i1p1_all.nc 
-
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/calc_zonal_aggregate.py /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/historical/yr/ocean/r1i1p1/so/latest/so-vertical-mean_Oyr_NorESM1-M_historical_r1i1p1_??????-??????.nc sea_water_salinity mean /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/historical/yr/ocean/r1i1p1/so/latest/so-vertical-zonal-mean_Oyr_NorESM1-M_historical_r1i1p1_all.nc --ref_file /g/data/ua6/DRSv3/CMIP5/NorESM1-M/historical/mon/atmos/r1i1p1/rsdt/latest/rsdt_Amon_NorESM1-M_historical_r1i1p1_185001-200512.nc toa_incoming_shortwave_flux
-
-/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python /home/599/dbi599/ocean-analysis/data_processing/calc_zonal_aggregate.py /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/so/latest/so-vertical-mean_Oyr_NorESM1-M_piControl_r1i1p1_??????-??????.nc sea_water_salinity mean /g/data/r87/dbi599/DRSv2/CMIP5/NorESM1-M/piControl/yr/ocean/r1i1p1/so/latest/so-vertical-zonal-mean_Oyr_NorESM1-M_piControl_r1i1p1_all.nc --ref_file /g/data/ua6/DRSv3/CMIP5/NorESM1-M/historical/mon/atmos/r1i1p1/rsdt/latest/rsdt_Amon_NorESM1-M_historical_r1i1p1_185001-200512.nc toa_incoming_shortwave_flux
-
-bash /home/599/dbi599/ocean-analysis/data_processing/calc_vertical_aggregate.sh /g/data/ua6/DRSv3/CMIP5/NorESM1-M/historical/mon/ocean/r1i1p1/so/latest/so_Omon_NorESM1-M_historical_r1i1p1_??????-??????.nc
-
-bash /home/599/dbi599/ocean-analysis/data_processing/calc_vertical_aggregate.sh /g/data/ua6/DRSv3/CMIP5/NorESM1-M/piControl/mon/ocean/r1i1p1/so/latest/so_Omon_NorESM1-M_piControl_r1i1p1_??????-??????.nc
+### Vertical aggregation
+#bash ${script_dir}/calc_vertical_aggregate.sh ${ua6_dir}/${experiment}/mon/ocean/r1i1p1/so/latest/so_Omon_${model}_${experiment}_r1i1p1_??????-??????.nc
+#bash ${script_dir}/calc_vertical_aggregate.sh ${ua6_dir}/piControl/mon/ocean/r1i1p1/so/latest/so_Omon_${model}_piControl_r1i1p1_??????-??????.nc
 
 
+### Control zonal mean
+control_dir=${r87_dir}/piControl/yr/ocean/r1i1p1/so/latest/
+mkdir -p ${control_dir}
 
+control_zonal_mean_file=${control_dir}/so-vertical-zonal-mean_Oyr_${model}_piControl_r1i1p1_all.nc
+control_zonal_mean_command="${python} ${script_dir}/calc_zonal_aggregate.py ${r87_dir}/piControl/yr/ocean/r1i1p1/so/latest/so-vertical-mean_Oyr_${model}_piControl_r1i1p1_??????-??????.nc sea_water_salinity mean ${control_zonal_mean_file} ${ref_file}"
+
+echo ${control_zonal_mean_command}
+${control_zonal_mean_command}
+
+### Experiment zonal mean
+experiment_zonal_mean_file=${r87_dir}/${experiment}/yr/ocean/r1i1p1/so/latest/so-zonal-vertical-mean_Oyr_${model}_${experiment}_r1i1p1_all.nc
+experiment_zonal_mean_command="${python} ${script_dir}/calc_zonal_aggregate.py ${r87_dir}/${experiment}/yr/ocean/r1i1p1/so/latest/so-vertical-mean_Oyr_${model}_${experiment}_r1i1p1_??????-??????.nc sea_water_salinity mean ${experiment_zonal_mean_file} ${ref_file}"
+
+#echo ${experiment_zonal_mean_command}
+#${experiment_zonal_mean_command}
+
+### Drift coefficients
+so_coefficient_file=${r87_dir}/piControl/yr/ocean/r1i1p1/so/latest/so-vertical-zonal-mean-coefficients_Oyr_${model}_piControl_r1i1p1_all.nc
+so_coefficient_command="${python} ${script_dir}/calc_drift_coefficients.py ${control_zonal_mean_file} sea_water_salinity ${so_coefficient_file}"
+
+#echo ${so_coefficient_command}
+#${so_coefficient_command}
+
+### Remove drift
+so_drift_command="${python} ${script_dir}/remove_drift.py ${experiment_zonal_mean_file} sea_water_salinity annual ${so_coefficient_file} ${r87_dir}/${experiment}/yr/ocean/r1i1p1/so/latest/dedrifted/so-vertical-zonal-mean_Oyr_${model}_${experiment}_r1i1p1_all.nc --no_data_check"
+
+#echo ${so_drift_command}
+#${so_drift_command}
 
