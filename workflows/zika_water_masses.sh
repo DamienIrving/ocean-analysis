447c8d7753d29f3f732421f3fde2977ac6d99d7a
Damien Irving <irving.damien@gmail.com>
2019-12-11 10:50:30 +1100

    Fixed some typos

--- workflows/zika_water_masses.sh
+++ workflows/zika_water_masses.sh
@@ -1,15 +1,16 @@
-institution=CCCma
-model=CanESM5
-cmip_version=v20190429
-scenario_version=v20190429
-fx_version=v20190429
+institution=MIROC
+model=MIROC-ES2L
+cmip_version=v20190823
+scenario_version=v20190823
+fx_version=v20190823
 grid=gn
-ripf=r1i1p1f1
-end_future_date=230012
+ripf=r1i1p1f2
+end_future_date=210012
+fx_exp=historical
+
 
 cmip_exp=historical
 scenario_exp=ssp585
-fx_exp=historical
 
 start_date=185001
 end_hist_date=201412
@@ -70,7 +71,7 @@ mkdir -p ${scenario_wm_dir}
 #${cmip_awm_command}
 
 #cmip_awmfiles=(`ls ${r87_cmip_dir}/${cmip_exp}/${ripf}/Omon/water-mass/${grid}/${cmip_version}/surface-water-mass*.nc`)
-#cmip_merge_command="${python} ${script_dir}/merge_files.py ${cmip_awmfiles[@]} /g/data/r87/dbi599/zika/surface-water-mass_Omon_${model}_${cmip_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc --variables cell_area Sea_Water_Salinity_times_Grid-Cell_Area Sea_Water_Potential_Temperature_times_Grid-Cell_Area"
+#cmip_merge_command="${python} ${script_dir}/merge_files.py ${cmip_awmfiles[@]} /g/data/r87/dbi599/zika/surface-water-mass_Omon_${model}_${cmip_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc --variables Grid-Cell_Area_binned_by_temperature Sea_Water_Salinity_times_Grid-Cell_Area_binned_by_temperature Sea_Water_Potential_Temperature_times_Grid-Cell_Area_binned_by_temperature Grid-Cell_Area_binned_by_salinity Sea_Water_Salinity_times_Grid-Cell_Area_binned_by_salinity Sea_Water_Potential_Temperature_times_Grid-Cell_Area_binned_by_salinity"
 #echo ${cmip_merge_command}
 #${cmip_merge_command}
 
@@ -79,7 +80,7 @@ mkdir -p ${scenario_wm_dir}
 #${scenario_awm_command}
 
 #scenario_awmfiles=(`ls ${r87_scenario_dir}/${scenario_exp}/${ripf}/Omon/water-mass/${grid}/${scenario_version}/surface-water-mass*.nc`)
-#scenario_merge_command="${python} ${script_dir}/merge_files.py ${scenario_awmfiles[@]} /g/data/r87/dbi599/zika/surface-water-mass_Omon_${model}_${scenario_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc --variables cell_area Sea_Water_Salinity_times_Grid-Cell_Area Sea_Water_Potential_Temperature_times_Grid-Cell_Area"
+#scenario_merge_command="${python} ${script_dir}/merge_files.py ${scenario_awmfiles[@]} /g/data/r87/dbi599/zika/surface-water-mass_Omon_${model}_${scenario_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc --variables Grid-Cell_Area_binned_by_temperature Sea_Water_Salinity_times_Grid-Cell_Area_binned_by_temperature Sea_Water_Potential_Temperature_times_Grid-Cell_Area_binned_by_temperature Grid-Cell_Area_binned_by_salinity Sea_Water_Salinity_times_Grid-Cell_Area_binned_by_salinity Sea_Water_Potential_Temperature_times_Grid-Cell_Area_binned_by_salinity"
 #echo ${scenario_merge_command}
 #${scenario_merge_command}
 
@@ -87,34 +88,34 @@ mkdir -p ${scenario_wm_dir}
 
 cmip_awm_file=${cmip_wm_dir}/surface-water-mass_Omon_${model}_${cmip_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc
 cmip_awm_command="${python} ${script_dir}/calc_water_mass_components.py ${areacello_file} ${basin_file} ${cmip_awm_file} --salinity_files ${cmip_sfiles[@]} --temperature_files ${cmip_tfiles[@]}"
-#echo ${cmip_awm_command}
-#${cmip_awm_command}
+echo ${cmip_awm_command}
+${cmip_awm_command}
 
 scenario_awm_file=${scenario_wm_dir}/surface-water-mass_Omon_${model}_${scenario_exp}_${ripf}_${grid}_201501-${end_future_date}.nc
 scenario_awm_command="${python} ${script_dir}/calc_water_mass_components.py ${areacello_file} ${basin_file} ${scenario_awm_file} --salinity_files ${scenario_sfiles[@]} --temperature_files ${scenario_tfiles[@]}"
-#echo ${scenario_awm_command}
-#${scenario_awm_command}
+echo ${scenario_awm_command}
+${scenario_awm_command}
 
 
 # Volume profiles
 
 ## Each file individually
 
-cmip_vwm_command="bash ${script_dir}/calc_water_mass_components_by_volume.sh ${volume_file} ${basin_file} ${cmip_tfiles[@]}"
+#cmip_vwm_command="bash ${script_dir}/calc_water_mass_components_by_volume.sh ${volume_file} ${basin_file} ${cmip_tfiles[@]}"
 #echo ${cmip_vwm_command}
 #${cmip_vwm_command}
 
-cmip_vwmfiles=(`ls ${r87_cmip_dir}/${cmip_exp}/${ripf}/Omon/water-mass/${grid}/${cmip_version}/water-mass*.nc`)
-cmip_merge_command="${python} ${script_dir}/merge_files.py ${cmip_vwmfiles[@]} /g/data/r87/dbi599/zika/water-mass_Omon_${model}_${cmip_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc --variables Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_temperature Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_salinity"
+#cmip_vwmfiles=(`ls ${r87_cmip_dir}/${cmip_exp}/${ripf}/Omon/water-mass/${grid}/${cmip_version}/water-mass*.nc`)
+#cmip_merge_command="${python} ${script_dir}/merge_files.py ${cmip_vwmfiles[@]} /g/data/r87/dbi599/zika/water-mass_Omon_${model}_${cmip_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc --variables Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_temperature Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_salinity"
 #echo ${cmip_merge_command}
 #${cmip_merge_command}
 
-scenario_vwm_command="bash ${script_dir}/calc_water_mass_components_by_volume.sh ${volume_file} ${basin_file} ${scenario_tfiles[@]}"
+#scenario_vwm_command="bash ${script_dir}/calc_water_mass_components_by_volume.sh ${volume_file} ${basin_file} ${scenario_tfiles[@]}"
 #echo ${scenario_vwm_command}
 #${scenario_vwm_command}
 
-scenario_vwmfiles=(`ls ${r87_scenario_dir}/${scenario_exp}/${ripf}/Omon/water-mass/${grid}/${scenario_version}/water-mass*.nc`)
-scenario_merge_command="${python} ${script_dir}/merge_files.py ${scenario_vwmfiles[@]} /g/data/r87/dbi599/zika/water-mass_Omon_${model}_${scenario_exp}_${ripf}_${grid}_201501-${end_future_date}.nc --variables Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_temperature Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_salinity"
+#scenario_vwmfiles=(`ls ${r87_scenario_dir}/${scenario_exp}/${ripf}/Omon/water-mass/${grid}/${scenario_version}/water-mass*.nc`)
+#scenario_merge_command="${python} ${script_dir}/merge_files.py ${scenario_vwmfiles[@]} /g/data/r87/dbi599/zika/water-mass_Omon_${model}_${scenario_exp}_${ripf}_${grid}_201501-${end_future_date}.nc --variables Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_temperature Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_temperature Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Salinity_times_Ocean_Grid-Cell_Volume_binned_by_salinity Sea_Water_Potential_Temperature_times_Ocean_Grid-Cell_Volume_binned_by_salinity"
 #echo ${scenario_merge_command}
 #${scenario_merge_command}
 
@@ -122,13 +123,13 @@ scenario_merge_command="${python} ${script_dir}/merge_files.py ${scenario_vwmfil
 
 cmip_vwm_file=${cmip_wm_dir}/water-mass_Omon_${model}_${cmip_exp}_${ripf}_${grid}_${start_date}-${end_hist_date}.nc
 cmip_vwm_command="${python} ${script_dir}/calc_water_mass_components.py ${volume_file} ${basin_file} ${cmip_vwm_file} --salinity_files ${cmip_sfiles[@]} --temperature_files ${cmip_tfiles[@]}"
-#echo ${cmip_vwm_command}
-#${cmip_vwm_command}
+echo ${cmip_vwm_command}
+${cmip_vwm_command}
 
 scenario_vwm_file=${scenario_wm_dir}/water-mass_Omon_${model}_${scenario_exp}_${ripf}_${grid}_201501-${end_future_date}.nc
 scenario_vwm_command="${python} ${script_dir}/calc_water_mass_components.py ${volume_file} ${basin_file} ${scenario_vwm_file} --salinity_files ${scenario_sfiles[@]} --temperature_files ${scenario_tfiles[@]}"
-#echo ${scenario_vwm_command}
-#${scenario_vwm_command}
+echo ${scenario_vwm_command}
+${scenario_vwm_command}
 
 
 # T-S distributions
@@ -172,11 +173,11 @@ echo ${cmip_wfo_tos_command}
 ${cmip_wfo_tos_command}
 
 
-#echo ${vdist_file}
-#echo ${adist_file}
-#echo ${cmip_vwm_file}
-#echo ${cmip_awm_file}
-#echo ${scenario_vwm_file}
-#echo ${scenario_awm_file}
-echo ${cmip_wfo_tos_file}
+#ls -l ${vdist_file}
+#ls -l ${adist_file}
+ls -l ${cmip_vwm_file}
+ls -l ${cmip_awm_file}
+ls -l ${scenario_vwm_file}
+ls -l ${scenario_awm_file}
+ls -l ${cmip_wfo_tos_file}
 
