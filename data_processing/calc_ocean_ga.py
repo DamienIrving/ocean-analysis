35877807b8a3809b21902e045a7b5b6533e4979c
Damien Irving <irving.damien@gmail.com>
2020-01-21 17:18:01 +1100

    Generalised to do thetaoga and soga

--- data_processing/calc_ocean_ga.py
+++ data_processing/calc_ocean_ga.py
@@ -1,7 +1,7 @@
 """
-Filename:     calc_thetaoga.py
+Filename:     calc_ocean_ga.py
 Author:       Damien Irving, irving.damien@gmail.com
-Description:  calculate thetoga from thetao
+Description:  calculate thetoga or soga from thetao or so data
 
 """
 
@@ -46,18 +46,20 @@ def save_history(cube, field, filename):
 def main(inargs):
     """Run the program."""
 
+    standard_names = {'thetao': 'sea_water_potential_temperature',
+                      'so': 'sea_water_salinity'}
     volume_data, volume_var, volume_atts, volume_obj = gio.get_ocean_weights(inargs.volfile)
     output_cubelist = iris.cube.CubeList([])
     for infile in inargs.infiles:
-        cube = iris.load_cube(infile, 'sea_water_potential_temperature')
+        cube = iris.load_cube(infile, standard_names[inargs.invar])
         weights = uconv.broadcast_array(volume_data, [1, 3], cube.shape)
         coord_names = [coord.name() for coord in cube.dim_coords]
         aux_coord_names = [coord.name() for coord in cube.aux_coords]
-        thetaoga = cube.collapsed(coord_names[1:], iris.analysis.MEAN, weights=weights)
+        ga = cube.collapsed(coord_names[1:], iris.analysis.MEAN, weights=weights)
         for coord in coord_names[1:] + aux_coord_names:
-            thetaoga.remove_coord(coord)
-        thetaoga.var_name = 'thetaoga'
-        output_cubelist.append(thetaoga)
+            ga.remove_coord(coord)
+        ga.var_name = inargs.invar + 'ga'
+        output_cubelist.append(ga)
         print(infile)
 
     outcube = gio.combine_cubes(output_cubelist)
@@ -77,13 +79,14 @@ author:
 
 """
 
-    description = 'Calculate thetaoga from thetao'
+    description = 'Calculate thetaoga or soga from thetao or so data'
     parser = argparse.ArgumentParser(description=description,
                                      epilog=extra_info, 
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
                                      
-    parser.add_argument("infiles", type=str, nargs='*', help="Input thetao files")
+    parser.add_argument("infiles", type=str, nargs='*', help="Input thetao or so files")
+    parser.add_argument("invar", type=str, choices=('thetao', 'so'), help="Input variable")
     parser.add_argument("volfile", type=str, help="Volume file")
     parser.add_argument("outfile", type=str, help="Output file")
 
