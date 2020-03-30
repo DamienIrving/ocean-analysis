e0bbdbe3d856a01180871142674b07f9b12e648a
Damien Irving <irving.damien@gmail.com>
2020-03-30 12:20:31 +1100

    Year axis dedrifting now functional

--- data_processing/remove_drift_year_axis.py
+++ data_processing/remove_drift_year_axis.py
@@ -13,7 +13,7 @@ import argparse
 import numpy
 import iris
 import cf_units
-import datetime
+import cmdline_provenance as cmdprov
 
 # Import my modules
 
@@ -36,29 +36,13 @@ except ImportError:
 
 # Define functions
 
-def check_data(new_cube, orig_cube, infile):
-    """Check that the new data is valid."""
-
-    orig_max = orig_cube.data.max()
-    orig_min = orig_cube.data.min()
-
-    new_max = new_cube.data.max()
-    new_min = new_cube.data.min()
-
-    valid_max = orig_max * 1.2 if orig_max > 0.0 else orig_max * 0.8
-    valid_min = orig_min * 1.2 if orig_min < 0.0 else orig_min * 0.8 
-
-    assert new_max < valid_max, 'New data max is %f, %s' %(new_max, infile)
-    assert new_min >= valid_min, 'New data min is %f, %s' %(new_min, infile)
-
-
-def get_branch_year(data_cube)
+def get_branch_year(data_cube):
     """Get the year of the branching in control run."""
 
     control_time_units = data_cube.attributes['parent_time_units']
     branch_time = data_cube.attributes['branch_time_in_parent']
     branch_datetime = cf_units.num2date(branch_time, control_time_units, cf_units.CALENDAR_STANDARD)
-    pdb.set_trace()
+    branch_year = branch_datetime.year
 
     return branch_year
 
@@ -77,18 +61,17 @@ def main(inargs):
     
     branch_year = get_branch_year(data_cube)
     time_values = numpy.arange(branch_year, branch_year + data_cube.shape[0]) 
-
     drift_signal, start_polynomial = remove_drift.apply_polynomial(time_values, coefficient_a_cube.data,
                                                                    coefficient_b_cube.data, coefficient_c_cube.data,
                                                                    coefficient_d_cube.data, poly_start=None)
 
     new_cube = data_cube - drift_signal
-    check_data(new_cube, data_cube, filename)
+    remove_drift.check_data(new_cube, data_cube, inargs.data_file)
     new_cube.metadata = data_cube.metadata
             
     metadata_dict = {inargs.data_file: data_cube.attributes['history'], 
                      inargs.coefficient_file: coefficient_a_cube.attributes['history']}
-    new_cube.attributes['history'] = gio.write_metadata(file_info=metadata_dict)
+    new_cube.attributes['history'] = cmdprov.new_log(infile_history=metadata_dict, git_repo=repo_dir)
     iris.save(new_cube, inargs.outfile)
 
 
@@ -112,7 +95,7 @@ notes:
 
     parser.add_argument("data_file", type=str, help="Input data file")
     parser.add_argument("var", type=str, help="Variable standard_name")
-    parser.add_argument("timescale", type=str, choices=('monthly', 'annual'), help="Timescale of input data")
+    parser.add_argument("coefficient_file", type=str, help="Drift coefficient file")
     parser.add_argument("outfile", type=str, help="outfile")
 
     args = parser.parse_args()            
