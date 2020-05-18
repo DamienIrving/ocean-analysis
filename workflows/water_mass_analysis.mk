af910e7f841413852b67ac1735e75fa4228b1345
Damien Irving <irving.damien@gmail.com>
2020-05-18 14:54:43 +1000

    Can now do wfo or hfds

--- workflows/water_mass_analysis.mk
+++ workflows/water_mass_analysis.mk
@@ -5,7 +5,7 @@
 # To execute:
 #   make all -n -B -f water_mass_analysis.mk  (-n is a dry run) (-B is a force make)
 #
-#   (Options besides all: volcello-tbin so-volcello-tbin surface wfo-anomaly)  
+#   (Options besides all: volcello-tbin so-volcello-tbin surface sf-anomaly)  
 
 include cmip_config.mk
 
@@ -18,11 +18,11 @@ SALINITY_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}
 TEMPERATURE_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/historical/${RUN}/Omon/thetao/${GRID}/${HIST_VERSION}/thetao*.nc))
 TEMPERATURE_FILE_HIST := $(firstword ${TEMPERATURE_FILES_HIST})
 TOS_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/historical/${RUN}/Omon/${TOS_VAR}/${GRID}/${HIST_VERSION}/${TOS_VAR}*.nc))
-WFO_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/historical/${RUN}/Omon/wfo/${GRID}/${HIST_VERSION}/wfo*.nc))
+SF_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/historical/${RUN}/Omon/${SF_VAR}/${GRID}/${HIST_VERSION}/${SF_VAR}*.nc))
 SALINITY_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/${RUN}/Omon/so/${GRID}/${CNTRL_VERSION}/so*.nc)) 
 TEMPERATURE_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/${RUN}/Omon/thetao/${GRID}/${CNTRL_VERSION}/thetao*.nc))
 TOS_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/${RUN}/Omon/${TOS_VAR}/${GRID}/${CNTRL_VERSION}/${TOS_VAR}*.nc))
-WFO_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/${RUN}/Omon/wfo/${GRID}/${CNTRL_VERSION}/wfo*.nc))
+SF_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/${RUN}/Omon/${SF_VAR}/${GRID}/${CNTRL_VERSION}/${SF_VAR}*.nc))
 
 
 # basin file
@@ -33,47 +33,48 @@ ${BASIN_FILE} : ${TEMPERATURE_FILE_HIST}
 	mkdir -p ${BASIN_DIR}
 	${PYTHON} ${DATA_SCRIPT_DIR}/calc_basin.py $< sea_water_potential_temperature $@
 
-# wfo(year, tos, basin), "Water Flux Into Sea Water"
 
-WFO_BINNED_DIR_HIST=${MY_DATA_DIR}/${INSTITUTION}/${MODEL}/historical/${RUN}/Omon/wfo/${GRID}/${HIST_VERSION}
-WFO_BINNED_FILE_HIST=${WFO_BINNED_DIR_HIST}/wfo-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}.nc
-${WFO_BINNED_FILE_HIST} : ${AREACELLO_FILE} ${BASIN_FILE}
-	mkdir -p ${WFO_BINNED_DIR_HIST}
-	${PYTHON} ${DATA_SCRIPT_DIR}/calc_surface_flux_histogram.py ${WFO_FILES_HIST} water_flux_into_sea_water $< $(word 2,$^) $@ --bin_files ${TOS_FILES_HIST} --bin_var ${TOS_LONG_NAME}
+# surface_flux(year, tos, basin)
 
-WFO_BINNED_DIR_CNTRL=${MY_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/${RUN}/Omon/wfo/${GRID}/${CNTRL_VERSION}
-WFO_BINNED_FILE_CNTRL=${WFO_BINNED_DIR_CNTRL}/wfo-${TOS_VAR}-binned_Omon_${MODEL}_piControl_${RUN}_${GRID}_${CNTRL_TIME}.nc
-${WFO_BINNED_FILE_CNTRL} : ${AREACELLO_FILE} ${BASIN_FILE}
-	mkdir -p ${WFO_BINNED_DIR_CNTRL}
-	${PYTHON} ${DATA_SCRIPT_DIR}/calc_surface_flux_histogram.py ${WFO_FILES_CNTRL} water_flux_into_sea_water $< $(word 2,$^) $@ --bin_files ${TOS_FILES_CNTRL} --bin_var ${TOS_LONG_NAME}
+SF_BINNED_DIR_HIST=${MY_DATA_DIR}/${INSTITUTION}/${MODEL}/historical/${RUN}/Omon/${SF_VAR}/${GRID}/${HIST_VERSION}
+SF_BINNED_FILE_HIST=${SF_BINNED_DIR_HIST}/${SF_VAR}-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}.nc
+${SF_BINNED_FILE_HIST} : ${AREACELLO_FILE} ${BASIN_FILE}
+	mkdir -p ${SF_BINNED_DIR_HIST}
+	${PYTHON} ${DATA_SCRIPT_DIR}/calc_surface_flux_histogram.py ${SF_FILES_HIST} ${SF_NAME} $< $(word 2,$^) $@ --bin_files ${TOS_FILES_HIST} --bin_var ${TOS_LONG_NAME}
+
+SF_BINNED_DIR_CNTRL=${MY_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/${RUN}/Omon/${SF_VAR}/${GRID}/${CNTRL_VERSION}
+SF_BINNED_FILE_CNTRL=${SF_BINNED_DIR_CNTRL}/${SF_VAR}-${TOS_VAR}-binned_Omon_${MODEL}_piControl_${RUN}_${GRID}_${CNTRL_TIME}.nc
+${SF_BINNED_FILE_CNTRL} : ${AREACELLO_FILE} ${BASIN_FILE}
+	mkdir -p ${SF_BINNED_DIR_CNTRL}
+	${PYTHON} ${DATA_SCRIPT_DIR}/calc_surface_flux_histogram.py ${SF_FILES_CNTRL} ${SF_NAME} $< $(word 2,$^) $@ --bin_files ${TOS_FILES_CNTRL} --bin_var ${TOS_LONG_NAME}
 
 ## cumulative sum
 
-WFO_BINNED_CUMSUM_FILE_HIST=${WFO_BINNED_DIR_HIST}/wfo-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}-cumsum.nc
-${WFO_BINNED_CUMSUM_FILE_HIST} : ${WFO_BINNED_FILE_HIST}
-	${PYTHON} ${DATA_SCRIPT_DIR}/calc_cumsum.py $< water_flux_into_sea_water $@ --flux_to_mag
+SF_BINNED_CUMSUM_FILE_HIST=${SF_BINNED_DIR_HIST}/${SF_VAR}-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}-cumsum.nc
+${SF_BINNED_CUMSUM_FILE_HIST} : ${SF_BINNED_FILE_HIST}
+	${PYTHON} ${DATA_SCRIPT_DIR}/calc_cumsum.py $< ${SF_NAME} $@ --flux_to_mag
 
-WFO_BINNED_CUMSUM_FILE_CNTRL=${WFO_BINNED_DIR_CNTRL}/wfo-${TOS_VAR}-binned_Omon_${MODEL}_piControl_${RUN}_${GRID}_${CNTRL_TIME}-cumsum.nc
-${WFO_BINNED_CUMSUM_FILE_CNTRL} : ${WFO_BINNED_FILE_CNTRL}
-	${PYTHON} ${DATA_SCRIPT_DIR}/calc_cumsum.py $< water_flux_into_sea_water $@ --flux_to_mag
+SF_BINNED_CUMSUM_FILE_CNTRL=${SF_BINNED_DIR_CNTRL}/${SF_VAR}-${TOS_VAR}-binned_Omon_${MODEL}_piControl_${RUN}_${GRID}_${CNTRL_TIME}-cumsum.nc
+${SF_BINNED_CUMSUM_FILE_CNTRL} : ${SF_BINNED_FILE_CNTRL}
+	${PYTHON} ${DATA_SCRIPT_DIR}/calc_cumsum.py $< ${SF_NAME} $@ --flux_to_mag
 
 ## remove drift / calculate anomaly
 
-WFO_BINNED_CUMSUM_COEFFICIENT_FILE=${WFO_BINNED_DIR_CNTRL}/wfo-${TOS_VAR}-binned-coefficients_Omon_${MODEL}_piControl_${RUN}_${GRID}_${CNTRL_TIME}-cumsum.nc
-${WFO_BINNED_CUMSUM_COEFFICIENT_FILE} : ${WFO_BINNED_CUMSUM_FILE_CNTRL}
-	${PYTHON} ${DATA_SCRIPT_DIR}/calc_drift_coefficients.py $< water_flux_into_sea_water $@ --no_data_check
+SF_BINNED_CUMSUM_COEFFICIENT_FILE=${SF_BINNED_DIR_CNTRL}/${SF_VAR}-${TOS_VAR}-binned-coefficients_Omon_${MODEL}_piControl_${RUN}_${GRID}_${CNTRL_TIME}-cumsum.nc
+${SF_BINNED_CUMSUM_COEFFICIENT_FILE} : ${SF_BINNED_CUMSUM_FILE_CNTRL}
+	${PYTHON} ${DATA_SCRIPT_DIR}/calc_drift_coefficients.py $< ${SF_NAME} $@ --no_data_check
 
-WFO_ANOMALY_BINNED_CUMSUM_FILE=${WFO_BINNED_DIR_HIST}/wfo-anomaly-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}-cumsum.nc
-${WFO_ANOMALY_BINNED_CUMSUM_FILE} : ${WFO_BINNED_CUMSUM_FILE_HIST} ${WFO_BINNED_CUMSUM_COEFFICIENT_FILE}
-	${PYTHON} ${DATA_SCRIPT_DIR}/remove_drift_year_axis.py $< water_flux_into_sea_water $(word 2,$^) $@ ${BRANCH_YEAR}
+SF_ANOMALY_BINNED_CUMSUM_FILE=${SF_BINNED_DIR_HIST}/${SF_VAR}-anomaly-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}-cumsum.nc
+${SF_ANOMALY_BINNED_CUMSUM_FILE} : ${SF_BINNED_CUMSUM_FILE_HIST} ${SF_BINNED_CUMSUM_COEFFICIENT_FILE}
+	${PYTHON} ${DATA_SCRIPT_DIR}/remove_drift_year_axis.py $< ${SF_NAME} $(word 2,$^) $@ ${BRANCH_YEAR}
 
 ## plot
 
-WFO_ANOMALY_BINNED_CUMSUM_PLOT=/g/data/r87/dbi599/temp/wfo-anomaly-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}-cumsum_bin34.png
-${WFO_ANOMALY_BINNED_CUMSUM_PLOT} : ${WFO_BINNED_CUMSUM_FILE_CNTRL} ${WFO_BINNED_CUMSUM_FILE_HIST} ${WFO_ANOMALY_BINNED_CUMSUM_FILE} ${WFO_BINNED_CUMSUM_COEFFICIENT_FILE}
-	${PYTHON} ${VIZ_SCRIPT_DIR}/plot_drift.py water_flux_into_sea_water $@ --control_files $< --experiment_files $(word 2,$^) --dedrifted_files $(word 3,$^) --coefficient_file $(word 4,$^) --grid_point 34 -1 ${BRANCH_YEAR}
+SF_ANOMALY_BINNED_CUMSUM_PLOT=/g/data/r87/dbi599/temp/${SF_VAR}-anomaly-${TOS_VAR}-binned_Omon_${MODEL}_historical_${RUN}_${GRID}_${HIST_TIME}-cumsum_bin34.png
+${SF_ANOMALY_BINNED_CUMSUM_PLOT} : ${SF_BINNED_CUMSUM_FILE_CNTRL} ${SF_BINNED_CUMSUM_FILE_HIST} ${SF_ANOMALY_BINNED_CUMSUM_FILE} ${SF_BINNED_CUMSUM_COEFFICIENT_FILE}
+	${PYTHON} ${VIZ_SCRIPT_DIR}/plot_drift.py ${SF_NAME} $@ --control_files $< --experiment_files $(word 2,$^) --dedrifted_files $(word 3,$^) --coefficient_file $(word 4,$^) --grid_point 34 -1 ${BRANCH_YEAR}
 
-wfo-anomaly : ${WFO_ANOMALY_BINNED_CUMSUM_PLOT}
+sf-anomaly : ${SF_ANOMALY_BINNED_CUMSUM_PLOT}
 
 
 # surface water mass variables
@@ -137,8 +138,8 @@ so-volcello-tbin : ${SOVOL_PLOT_FILE}
 
 # targets
 
-all : wfo-anomaly surface volcello-tbin so-volcello-tbin 
-	echo ${WFO_ANOMALY_BINNED_CUMSUM_PLOT}
+all : sf-anomaly surface volcello-tbin so-volcello-tbin 
+	echo ${SF_ANOMALY_BINNED_CUMSUM_PLOT}
 	echo ${SURFACE_WATER_MASS_FILE_HIST}
 	echo ${VOL_PLOT_FILE}
 	echo ${SOVOL_PLOT_FILE}
