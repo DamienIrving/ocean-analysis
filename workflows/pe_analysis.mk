80587625765a1b8ac7bbaadbcaf3c61fdd13fb21
Damien Irving <irving.damien@gmail.com>
2020-06-10 10:49:30 +1000

    Updated with new config

--- workflows/pe_analysis.mk
+++ workflows/pe_analysis.mk
@@ -6,32 +6,64 @@
 #   make all -n -B -f pe_analysis.mk  (-n is a dry run) (-B is a force make)
 #
 
-include cmip_config.mk
+#include cmip_config.mk
 
+PYTHON=/g/data/r87/dbi599/miniconda3/envs/ocean/bin/python
+DATA_SCRIPT_DIR=/home/599/dbi599/ocean-analysis/data_processing
+VIZ_SCRIPT_DIR=/home/599/dbi599/ocean-analysis/visualisation
+
+MY_DATA_DIR=/g/data/r87/dbi599
+CMIP6_DATA_DIR=/g/data/oi10/replicas
+CMIP5_DATA_DIR=/g/data/al33/replicas
+
+PROJECT=CMIP6
+MIP=CMIP
+## CMIP DAMIP
+INSTITUTION=BCC
+MODEL=BCC-CSM2-MR
+EXPERIMENT=historical
+## hist-aer historical hist-GHG
+FX_EXP=historical
+HIST_RUN=r1i1p1f1
+CNTRL_RUN=r1i1p1f1
+FX_RUN=r1i1p1f1
+GRID=gn
+OFX_VERSION=v20181126
+ATMOS_HIST_VERSION=v20181126
+# v20190507 (hist-aer), v20190426 (hist-GHG), v20181126 (historical)
+HIST_VERSION=v20181126
+HIST_TIME=185001-201412
+## 185001-201412 (historical), 185001-202012 (DAMIP)
+CNTRL_VERSION=v20181015
+ATMOS_CNTRL_VERSION=v20181016
+CNTRL_TIME=185001-244912
+AREACELLO_DIR=${CMIP6_DATA_DIR}
+AREACELLA_DIR=${CMIP6_DATA_DIR}
+VOLCELLO_DIR=${MY_DATA_DIR}
 
 # File definitions, CMIP6 models
  
-PR_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Amon/pr/${GRID}/${ATMOS_HIST_VERSION}/pr*.nc))
-EVAP_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Amon/evspsbl/${GRID}/${ATMOS_HIST_VERSION}/evspsbl*.nc))
-PR_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Amon/pr/${GRID}/${ATMOS_CNTRL_VERSION}/pr*.nc))
-EVAP_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Amon/evspsbl/${GRID}/${ATMOS_CNTRL_VERSION}/evspsbl*.nc))
+PR_FILES_HIST := $(sort $(wildcard ${CMIP6_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Amon/pr/${GRID}/${ATMOS_HIST_VERSION}/pr*.nc))
+EVAP_FILES_HIST := $(sort $(wildcard ${CMIP6_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Amon/evspsbl/${GRID}/${ATMOS_HIST_VERSION}/evspsbl*.nc))
+PR_FILES_CNTRL := $(sort $(wildcard ${CMIP6_DATA_DIR}/${PROJECT}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Amon/pr/${GRID}/${ATMOS_CNTRL_VERSION}/pr*.nc))
+EVAP_FILES_CNTRL := $(sort $(wildcard ${CMIP6_DATA_DIR}/${PROJECT}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Amon/evspsbl/${GRID}/${ATMOS_CNTRL_VERSION}/evspsbl*.nc))
 
 # File definitions, CMIP5 models
 
-#PR_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/mon/atmos/Amon/${HIST_RUN}/${ATMOS_HIST_VERSION}/pr/pr*.nc))
-#EVAP_FILES_HIST := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/mon/atmos/Amon/${HIST_RUN}/${ATMOS_HIST_VERSION}/evspsbl/evspsbl*.nc))
-#PR_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/mon/atmos/Amon/${CNTRL_RUN}/${ATMOS_CNTRL_VERSION}/pr/pr*.nc))
-#EVAP_FILES_CNTRL := $(sort $(wildcard ${NCI_DATA_DIR}/${INSTITUTION}/${MODEL}/piControl/mon/atmos/Amon/${CNTRL_RUN}/${ATMOS_CNTRL_VERSION}/evspsbl/evspsbl*.nc))
+#PR_FILES_HIST := $(sort $(wildcard ${CMIP5_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/mon/atmos/Amon/${HIST_RUN}/${ATMOS_HIST_VERSION}/pr/pr*.nc))
+#EVAP_FILES_HIST := $(sort $(wildcard ${CMIP5_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/mon/atmos/Amon/${HIST_RUN}/${ATMOS_HIST_VERSION}/evspsbl/evspsbl*.nc))
+#PR_FILES_CNTRL := $(sort $(wildcard ${CMIP5_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/piControl/mon/atmos/Amon/${CNTRL_RUN}/${ATMOS_CNTRL_VERSION}/pr/pr*.nc))
+#EVAP_FILES_CNTRL := $(sort $(wildcard ${CMIP5_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/piControl/mon/atmos/Amon/${CNTRL_RUN}/${ATMOS_CNTRL_VERSION}/evspsbl/evspsbl*.nc))
 
 # P-E
 
-PE_MON_DIR_HIST=${MY_EXP_DATA_DIR}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Amon/pe/${GRID}/${ATMOS_HIST_VERSION}
+PE_MON_DIR_HIST=${MY_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Amon/pe/${GRID}/${ATMOS_HIST_VERSION}
 PE_FILE_HIST=${PE_MON_DIR_HIST}/pe_Amon_${MODEL}_${EXPERIMENT}_${HIST_RUN}_${GRID}_${HIST_TIME}.nc
 ${PE_FILE_HIST} :
 	mkdir -p ${PE_MON_DIR_HIST}
 	${PYTHON} ${DATA_SCRIPT_DIR}/calc_pe.py $@ --pr_files ${PR_FILES_HIST} --evap_files ${EVAP_FILES_HIST}
 
-PE_MON_DIR_CNTRL=${MY_CNTRL_DATA_DIR}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Amon/pe/${GRID}/${ATMOS_CNTRL_VERSION}
+PE_MON_DIR_CNTRL=${MY_DATA_DIR}/${PROJECT}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Amon/pe/${GRID}/${ATMOS_CNTRL_VERSION}
 PE_FILE_CNTRL=${PE_MON_DIR_CNTRL}/pe_Amon_${MODEL}_piControl_${CNTRL_RUN}_${GRID}_${CNTRL_TIME}.nc
 ${PE_FILE_CNTRL} :
 	mkdir -p ${PE_MON_DIR_CNTRL}
@@ -41,13 +73,13 @@ ${PE_FILE_CNTRL} :
 
 ## zonal sum
 
-PE_YR_DIR_HIST=${MY_EXP_DATA_DIR}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Ayr/pe/${GRID}/${ATMOS_HIST_VERSION}
+PE_YR_DIR_HIST=${MY_DATA_DIR}/${PROJECT}/${MIP}/${INSTITUTION}/${MODEL}/${EXPERIMENT}/${HIST_RUN}/Ayr/pe/${GRID}/${ATMOS_HIST_VERSION}
 PE_ZS_FILE_HIST=${PE_YR_DIR_HIST}/pe-zonal-sum_Ayr_${MODEL}_${EXPERIMENT}_${HIST_RUN}_${GRID}_${HIST_TIME}.nc
 ${PE_ZS_FILE_HIST} : ${PE_FILE_HIST}
 	mkdir -p ${PE_YR_DIR_HIST}
 	${PYTHON} ${DATA_SCRIPT_DIR}/calc_horizontal_aggregate.py $< precipitation_minus_evaporation_flux zonal sum $@ --multiply_by_area --annual --flux_to_mag
 
-PE_YR_DIR_CNTRL=${MY_CNTRL_DATA_DIR}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Ayr/pe/${GRID}/${ATMOS_CNTRL_VERSION}
+PE_YR_DIR_CNTRL=${MY_DATA_DIR}/${PROJECT}/CMIP/${INSTITUTION}/${MODEL}/piControl/${CNTRL_RUN}/Ayr/pe/${GRID}/${ATMOS_CNTRL_VERSION}
 PE_ZS_FILE_CNTRL=${PE_YR_DIR_CNTRL}/pe-zonal-sum_Ayr_${MODEL}_piControl_${CNTRL_RUN}_${GRID}_${CNTRL_TIME}.nc
 ${PE_ZS_FILE_CNTRL} : ${PE_FILE_CNTRL}
 	mkdir -p ${PE_YR_DIR_CNTRL}
@@ -87,7 +119,7 @@ ${PE_CUMSUM_HIST} : ${PE_FILE_HIST}
 	${PYTHON} ${DATA_SCRIPT_DIR}/calc_cumsum.py $< precipitation_minus_evaporation_flux $@ --annual --flux_to_mag
 
 PE_CUMSUM_CNTRL=${PE_YR_DIR_CNTRL}/pe_Ayr_${MODEL}_piControl_${CNTRL_RUN}_${GRID}_${CNTRL_TIME}-cumsum.nc
-${PE_CUMSUM_HIST} : ${PE_FILE_HIST}
+${PE_CUMSUM_CNTRL} : ${PE_FILE_CNTRL}
 	${PYTHON} ${DATA_SCRIPT_DIR}/calc_cumsum.py $< precipitation_minus_evaporation_flux $@ --annual --flux_to_mag
 
 ## cumulative anomaly
