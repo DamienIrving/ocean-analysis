036b37e33eb4e595ea9659699353e8275d81b3af
Damien Irving <irving.damien@gmail.com>
2020-08-14 14:01:38 +1000

    Allowing for P and E on their own

--- visualisation/water_cycle/plot_pe_region_ensemble.py
+++ visualisation/water_cycle/plot_pe_region_ensemble.py
@@ -33,10 +33,15 @@ except ImportError:
 
 # Define functions
 
-def get_data(infile, time_constraint):
+names = {'precipitation_minus_evaporation_flux': 'P-E',
+         'precipitation_flux': 'precipitation',
+         'water_evapotranspiration_flux': 'evaporation'}
+
+
+def get_data(infile, var, time_constraint):
     """Get the data for a particular model"""
     
-    cube, history = gio.combine_files(infile, 'precipitation minus evaporation flux', new_calendar='365_day')
+    cube, history = gio.combine_files([infile], var, new_calendar='365_day')
     cube = cube.extract(time_constraint)
     iris.coord_categorisation.add_year(cube, 'time')
     anomaly_data = cube.data - cube.data[0, :]
@@ -45,7 +50,7 @@ def get_data(infile, time_constraint):
     return cube, anomaly_data, start_data
 
 
-def plot_ensemble_lines(df, model_list, experiment):
+def plot_ensemble_lines(df, var, model_list, experiment):
     """Plot regional changes for each model as a line graph"""
 
     xvals = np.array([1, 2, 3, 4, 5])
@@ -64,7 +69,8 @@ def plot_ensemble_lines(df, model_list, experiment):
             yvals = df[(df['model'] == model_name)][var_name].values[1:]
             linestyle = '-' if model_num < 10 else '--'
             axes[plot_num].plot(xvals, yvals, label=model_name, marker='o', linestyle=linestyle)
-        ylabel = 'Accumulated P-E anomaly (kg)' if 'cumulative' in var_name else 'Change in P-E (%)'
+        short_name = names[var]
+        ylabel = f'Accumulated {short_name} anomaly (kg)' if 'cumulative' in var_name else f'Change in {short_name} (%)'
         axes[plot_num].set_ylabel(ylabel)
         axes[plot_num].set_xticks(xvals)
         axes[plot_num].set_xticklabels(['SH precip', 'SH evap', 'trop precip', 'NH evap', 'NH precip'])
@@ -90,16 +96,23 @@ def main(inargs):
     data = []
     model_list = []
     for infile in inargs.infiles:
-        cube, anomaly_data, start = get_data(infile, time_constraint)
+        cube, anomaly_data, start = get_data(infile, inargs.var, time_constraint)
         model = cube.attributes['source_id'] if 'source_id' in cube.attributes else cube.attributes['model_id']
         cum_change = anomaly_data[-1, :]
         ntimes = anomaly_data.shape[0]
         pct_change = ((cum_change / ntimes) / start) * 100
-        mean_cum_change = cum_change[0::2].mean()
-        total_cum_change = cum_change[0::2].sum()
-        total_start = start[0::2].sum()
-        total_pct_change = ((total_cum_change / ntimes) / total_start) * 100 
-        cum_change_anomaly = (cum_change * np.array([1, -1, 1, -1, 1])) - mean_cum_change
+        if inargs.var == 'precipitation_minus_evaporation_flux':
+            mean_cum_change = cum_change[0::2].mean()
+            total_cum_change = cum_change[0::2].sum()
+            total_start = start[0::2].sum()
+            cum_change_anomaly = (cum_change * np.array([1, -1, 1, -1, 1])) - mean_cum_change
+        else:
+            mean_cum_change = cum_change.mean()
+            total_cum_change = cum_change.sum()
+            total_start = start.sum()
+            cum_change_anomaly = cum_change - mean_cum_change
+
+        total_pct_change = ((total_cum_change / ntimes) / total_start) * 100
         pct_change_anomaly = pct_change - total_pct_change
 
         model_list.append(model)
@@ -113,12 +126,17 @@ def main(inargs):
     df = pd.DataFrame(data, columns = ['model', 'region', 'start',
                                        'cumulative_change', 'cumulative_change_anomaly',
                                        'percentage_change', 'percentage_change_anomaly'])
-    df['cumulative_change_sign_fix'] = df['cumulative_change'].where((df['region'] == 'SH precip') | \
-                                      (df['region'] == 'tropical precip') | (df['region'] == 'NH precip') | \
-                                      (df['region'] == 'globe precip'), df['cumulative_change'] * -1)
+
+    if inargs.var == 'precipitation_minus_evaporation_flux':
+        df['cumulative_change_sign_fix'] = df['cumulative_change'].where((df['region'] == 'SH precip') | \
+                                          (df['region'] == 'tropical precip') | (df['region'] == 'NH precip') | \
+                                          (df['region'] == 'globe precip'), df['cumulative_change'] * -1)
+    else:
+        df['cumulative_change_sign_fix'] = df['cumulative_change']
+
     model_list.sort()
     experiment = cube.attributes['experiment_id']
-    plot_ensemble_lines(df, model_list, experiment)
+    plot_ensemble_lines(df, inargs.var, model_list, experiment)
 
     plt.savefig(inargs.outfile, bbox_inches='tight')
     gio.write_metadata(inargs.outfile, file_info={inargs.infiles[-1]: cube.attributes['history']})
@@ -145,6 +163,9 @@ example:
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
     parser.add_argument("infiles", type=str, nargs='*', help="files for a particular experiment")
+    parser.add_argument("var", type=str, help="Variable name",
+                        choices=('precipitation_minus_evaporation_flux', 'precipitation_flux',
+                                 'water_evapotranspiration_flux'))
     parser.add_argument("outfile", type=str, help="Output file")
 
     parser.add_argument("--time_bounds", type=str, nargs=2, metavar=('START_DATE', 'END_DATE'),
