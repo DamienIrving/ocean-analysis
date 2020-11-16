83ce165bbecf00891fc288d33a2138a8b6806591
Damien Irving <irving.damien@gmail.com>
2020-11-17 10:33:25 +1100

    Can now handle FAFMIP files

--- data_processing/calc_binned_flux_sum.py
+++ data_processing/calc_binned_flux_sum.py
@@ -32,32 +32,48 @@ except ImportError:
 
 # Define functions
 
+long_names = {'sfch': 'surface heat fluxes',
+              'sfcv': 'surface heat fluxes from surface volume fluxes',
+              'vmix': 'vertical mixing',
+              'smix': 'miscellaneous mixing',
+              'rmix': 'neutral diffusion',
+              'ten': 'total tendency',
+              'sfc': 'total surface forcing',
+              'mix': 'total explicit mixing',
+             }
+
 def main(inargs):
     """Run the program."""
 
-    long_names = {'sfch': 'surface heat fluxes',
-                  'sfcv': 'surface heat fluxes from surface volume fluxes',
-                  'vmix': 'vertical mixing',
-                  'smix': 'miscellaneous mixing',
-                  'rmix': 'neutral diffusion',
-                  'ten': 'total tendency',
-                  'sfc': 'total surface forcing',
-                  'mix': 'total explicit mixing'}
-
     assert len(inargs.infiles) == len(inargs.invars)
-    cube = iris.load_cube(inargs.infiles[0], gio.check_iris_var(inargs.invars[0]))
-    for file_name, file_var in zip(inargs.infiles[1:], inargs.invars[1:]):
-        next_cube = iris.load_cube(file_name, gio.check_iris_var(file_var))
-        cube.data = cube.data + next_cube.data
+    cube_list = iris.cube.CubeList([])
+    bin_vars = ['tbin', 'sbin', 'tsbin']
+    bin_names = ['temperature', 'salinity', 'temperature and salinity']
+    for bin_var, bin_name in zip(bin_vars, bin_names):
+        running_sum = 0
+        metadata_dict = {}
+        for file_name, base_var in zip(inargs.infiles, inargs.invars):
+            cube = iris.load_cube(file_name, f'{base_var} binned by {bin_name}')
+            metadata_dict[file_name] = cube.attributes['history']
+            running_sum = running_sum + cube.data
+        cube.data = running_sum
+        if inargs.ref_file:
+            ref_cube = iris.load_cube(inargs.ref_file, inargs.new_var)
+            cube.attributes = ref_cube.attributes
+            cube.var_name = ref_cube.var_name + '_' + bin_var
+            cube.long_name = ref_cube.long_name + ' binned by ' + bin_name
+        else:
+            assert new_var in long_names.keys()
+            cube.var_name = inargs.new_var + '_' + bin_var
+            cube.long_name = long_names[inargs.new_var] + ' binned by ' + bin_name
+            assert cube.units == 'W'
+        cube_list.append(cube)
 
-    cube.var_name = inargs.new_var
-    cube.long_name = long_names[inargs.new_var]
-    cube.standard_name = None
-    assert cube.units == 'W'
-    log = cmdprov.new_log(infile_history={inargs.infiles[0]: cube.attributes['history']}, git_repo=repo_dir)
-    cube.attributes['history'] = log
+    log = cmdprov.new_log(infile_history=metadata_dict, git_repo=repo_dir)
+    for cube in cube_list:
+        cube.attributes['history'] = log
 
-    iris.save(cube, inargs.new_file)
+    iris.save(cube_list, inargs.new_file)
 
 
 if __name__ == '__main__':
@@ -81,6 +97,7 @@ author:
     parser.add_argument("new_file", type=str, help="New output file")
     
     parser.add_argument("--invars", type=str, nargs='*', help="Input variables")
+    parser.add_argument("--ref_file", type=str, help="reference cube for new file attributes")
 
     args = parser.parse_args()            
     main(args)
