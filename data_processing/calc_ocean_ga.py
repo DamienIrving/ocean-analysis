6e963a53e6dfd21ba992c4e68b24eccafe6c1e96
Damien Irving <irving.damien@gmail.com>
2021-06-30 15:46:34 +1000

    Slice by time

--- data_processing/calc_ocean_ga.py
+++ data_processing/calc_ocean_ga.py
@@ -1,46 +1,25 @@
-"""
-Filename:     calc_ocean_ga.py
-Author:       Damien Irving, irving.damien@gmail.com
-Description:  calculate thetoga or soga from thetao or so data
+"""Calculate thetaoga or soga from thetao or so data"""
 
-"""
-
-# Import general Python modules
-
-import sys, os, pdb, re
+import sys
+script_dir = sys.path[0]
+import os
+import pdb
+import re
 import argparse
+
 import iris
+import iris.util
 import cmdline_provenance as cmdprov
 
-# Import my modules
 
-cwd = os.getcwd()
-repo_dir = '/'
-for directory in cwd.split('/')[1:]:
-    repo_dir = os.path.join(repo_dir, directory)
-    if directory == 'ocean-analysis':
-        break
-
-modules_dir = os.path.join(repo_dir, 'modules')
-sys.path.append(modules_dir)
+repo_dir = '/'.join(script_dir.split('/')[:-1])
+module_dir = repo_dir + '/modules'
+sys.path.append(module_dir)
 try:
     import general_io as gio
     import convenient_universal as uconv
 except ImportError:
-    raise ImportError('Must run this script from anywhere within the ocean-analysis git repo')
-
-
-# Define functions
-
-history = []
-
-def save_history(cube, field, filename):
-    """Save the history attribute when reading the data.
-    (This is required because the history attribute differs between input files 
-      and is therefore deleted upon equilising attributes)  
-    """ 
-
-    history.append(cube.attributes['history'])
+    raise ImportError('Script and modules in wrong directories')
 
 
 def main(inargs):
@@ -50,38 +29,28 @@ def main(inargs):
                       'so': 'sea_water_salinity'}
     volume_cube = gio.get_ocean_weights(inargs.volfile)
     output_cubelist = iris.cube.CubeList([])
-    for infile in inargs.infiles:
-        cube = iris.load_cube(infile, standard_names[inargs.invar])
-        weights = uconv.broadcast_array(volume_cube.data, [1, 3], cube.shape)
+
+    cube, history = gio.combine_files(inargs.infiles, standard_names[inargs.invar], checks=True)
+    ntsteps = cube.shape[0]
+    for tstep, cube_slice in enumerate(cube.slices_over('time')):
+        print(f'time step {tstep + 1} of {ntsteps}')
         coord_names = [coord.name() for coord in cube.dim_coords]
         aux_coord_names = [coord.name() for coord in cube.aux_coords]
-        ga = cube.collapsed(coord_names[1:], iris.analysis.MEAN, weights=weights)
+        ga = cube_slice.collapsed(coord_names[1:], iris.analysis.MEAN, weights=volume_cube.data)
         for coord in coord_names[1:] + aux_coord_names:
             ga.remove_coord(coord)
-        ga.var_name = inargs.invar + 'ga'
+            ga.var_name = inargs.invar + 'ga'
         output_cubelist.append(ga)
-        print(infile)
-
-    outcube = gio.combine_cubes(output_cubelist)
+    outcube = output_cubelist.merge()[0]
     metadata_dict = {}
-    metadata_dict[infile] = cube.attributes['history'] 
+    metadata_dict[inargs.infiles[0]] = history 
     metadata_dict[inargs.volfile] = volume_cube.attributes['history'] 
     outcube.attributes['history'] = cmdprov.new_log(infile_history=metadata_dict, git_repo=repo_dir)
     iris.save(outcube, inargs.outfile)
         
 
 if __name__ == '__main__':
-
-    extra_info =""" 
-
-author:
-    Damien Irving, irving.damien@gmail.com
-
-"""
-
-    description = 'Calculate thetaoga or soga from thetao or so data'
-    parser = argparse.ArgumentParser(description=description,
-                                     epilog=extra_info, 
+    parser = argparse.ArgumentParser(description=__doc__,
                                      argument_default=argparse.SUPPRESS,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
                                      
@@ -89,6 +58,9 @@ author:
     parser.add_argument("invar", type=str, choices=('thetao', 'so'), help="Input variable")
     parser.add_argument("volfile", type=str, help="Volume file")
     parser.add_argument("outfile", type=str, help="Output file")
+
+    parser.add_argument("--time", type=str, nargs=2, default=None, metavar=('START_DATE', 'END_DATE'),
+                        help="Time period [default = entire]")
 
     args = parser.parse_args()
     main(args)
