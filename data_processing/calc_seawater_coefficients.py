dffef127329b2bb62575310e7e67511fbfc10bd8
Damien Irving <irving.damien@gmail.com>
2021-09-29 09:20:04 +1000

    Automatic handling of thetao and bigthetao

--- data_processing/calc_seawater_coefficients.py
+++ data_processing/calc_seawater_coefficients.py
@@ -21,10 +21,10 @@ import convenient_universal as uconv
 def _main(args):
     """Run the command line program."""
 
-    bigthetao_cube, bigthetao_history = gio.combine_files(args.temperature_file, 'sea_water_conservative_temperature', checks=True)
-    so_cube, so_history = gio.combine_files(args.salinity_file, 'sea_water_salinity', checks=True)
+    temperature_cube, temperature_history = gio.combine_files(args.temperature_file, args.temperature_var, checks=True)
+    salinity_cube, salinity_history = gio.combine_files(args.salinity_file, 'sea_water_salinity', checks=True)
    
-    assert 'c' in str(bigthetao_cube.units).lower(), "Input temperature units must be in celsius"
+    assert 'c' in str(temperature_cube.units).lower(), "Input temperature units must be in celsius"
 #    if not 'C' in str(bigthetao_cube.units):
 #        bigthetao_cube.data = bigthetao_cube.data - 273.15
 #        data_median = np.ma.median(bigthetao_cube.data)
@@ -32,16 +32,20 @@ def _main(args):
 #        assert data_median > -10
 #        bigthetao_cube.units = 'C'
 
-    target_shape = bigthetao_cube.shape[1:]
-    depth = bigthetao_cube.coord('depth').points * -1
+    target_shape = temperature_cube.shape[1:]
+    depth = temperature_cube.coord('depth').points * -1
     broadcast_depth = uconv.broadcast_array(depth, 0, target_shape)
-    broadcast_longitude = uconv.broadcast_array(bigthetao_cube.coord('longitude').points, [1, 2], target_shape)
-    broadcast_latitude = uconv.broadcast_array(bigthetao_cube.coord('latitude').points, [1, 2], target_shape)
+    broadcast_longitude = uconv.broadcast_array(temperature_cube.coord('longitude').points, [1, 2], target_shape)
+    broadcast_latitude = uconv.broadcast_array(temperature_cube.coord('latitude').points, [1, 2], target_shape)
     pressure = gsw.p_from_z(broadcast_depth, broadcast_latitude)
 
     absolute_salinity = gsw.SA_from_SP(so_cube.data, pressure, broadcast_longitude, broadcast_latitude)
-    conservative_temperature = bigthetao_cube.data  
-#   conservative_temperature = gsw.CT_from_pt(absolute_salinity, thetao_cube.data)
+    if args.temperature_var == 'sea_water_conservative_temperature':
+        conservative_temperature = temperature_cube.data
+    elif args.temperature_var == 'sea_water_potential_temperature':  
+        conservative_temperature = gsw.CT_from_pt(absolute_salinity, temperature_cube.data)
+    else:
+        raise ValueError('Invalid temperature variable')
 
     if args.coefficient == 'alpha':
         coefficient_data = gsw.alpha(absolute_salinity, conservative_temperature, pressure)
@@ -59,7 +63,7 @@ def _main(args):
         raise ValueError('Coefficient must be alpha or beta')
 
     iris.std_names.STD_NAMES[standard_name] = {'canonical_units': units}
-    coefficient_cube = bigthetao_cube.copy()
+    coefficient_cube = temperature_cube.copy()
     coefficient_cube.data = coefficient_data
     coefficient_cube.standard_name = standard_name    
     coefficient_cube.long_name = long_name
@@ -76,12 +80,11 @@ if __name__ == '__main__':
 
     parser.add_argument("salinity_file", type=str, help="Input seawater salinity file")                                     
     parser.add_argument("temperature_file", type=str, help="Input seawater temperature file")
+    parser.add_argument("temperature_var", type=str, help="Input seawater temperature variable",
+                        choices=('sea_water_conservative_temperature', 'sea_water_potential_temperature'))
     parser.add_argument("coefficient", type=str, choices=('alpha', 'beta'),
                         help='Calculate thermal expansion (alpha) or saline contraction (beta) coefficient')
     parser.add_argument("outfile", type=str, help="Output file")
-
-    parser.add_argument("--dask_config", type=str,
-                        help="YAML file specifying dask client configuration")
     
     args = parser.parse_args()
     _main(args)
--- workflows/calc_slr_coefficients.sh
+++ workflows/calc_slr_coefficients.sh
@@ -5,7 +5,7 @@
 
 function usage {
     echo "USAGE: bash $0 temperature_files"
-    echo "   temperature_files:      Input temprature files"
+    echo "   temperature_files:      Input temperature files"
     exit 1
 }
 
@@ -14,18 +14,24 @@ script_dir=/home/599/dbi599/ocean-analysis/data_processing
 
 temperature_files=($@)
 coefficients=(alpha beta)
+temperature_var=`basename $1 | cut -d _ -f1`
 
+if [ "${temperature_var}" == "bigthetao" ]; then
+    long_name=sea_water_conservative_temperature
+else
+    long_name=sea_water_potential_temperature
+fi
 
 for temperature_file in "${temperature_files[@]}"; do
-    salinity_file=`echo ${temperature_file} | sed s:bigthetao:so:g`
+    salinity_file=`echo ${temperature_file} | sed s:${temperature_var}:so:g`
 
     for coefficient in "${coefficients[@]}"; do
-        coefficient_file=`echo ${temperature_file} | sed s:bigthetao:${coefficient}:g`
+        coefficient_file=`echo ${temperature_file} | sed s:${temperature_var}:${coefficient}:g`
         coefficient_file=`echo ${coefficient_file} | sed s:fs38/publications:e14/dbi599:`
         directory=`dirname ${coefficient_file}`
 
     command1="mkdir -p ${directory}" 
-    command2="${python} ${script_dir}/calc_seawater_coefficients.py ${salinity_file} ${temperature_file} ${coefficient} ${coefficient_file}"
+    command2="${python} ${script_dir}/calc_seawater_coefficients.py ${salinity_file} ${temperature_file} ${long_name} ${coefficient} ${coefficient_file}"
 
     echo ${command1}
     echo ${command2}
