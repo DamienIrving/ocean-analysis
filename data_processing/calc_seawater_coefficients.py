b2f4275cb181a5c6f99ad415706e2b25172eeb77
Damien Irving <irving.damien@gmail.com>
2021-09-28 16:22:31 +1000

    Convert to absolute salinity

--- data_processing/calc_seawater_coefficients.py
+++ data_processing/calc_seawater_coefficients.py
@@ -23,21 +23,34 @@ def _main(args):
 
     bigthetao_cube, bigthetao_history = gio.combine_files(args.temperature_file, 'sea_water_conservative_temperature', checks=True)
     so_cube, so_history = gio.combine_files(args.salinity_file, 'sea_water_salinity', checks=True)
+   
+    assert 'c' in str(bigthetao_cube.units).lower(), "Input temperature units must be in celsius"
+#    if not 'C' in str(bigthetao_cube.units):
+#        bigthetao_cube.data = bigthetao_cube.data - 273.15
+#        data_median = np.ma.median(bigthetao_cube.data)
+#        assert data_median < 100
+#        assert data_median > -10
+#        bigthetao_cube.units = 'C'
 
     target_shape = bigthetao_cube.shape[1:]
     depth = bigthetao_cube.coord('depth').points * -1
     broadcast_depth = uconv.broadcast_array(depth, 0, target_shape)
+    broadcast_longitude = uconv.broadcast_array(bigthetao_cube.coord('longitude').points, [1, 2], target_shape)
     broadcast_latitude = uconv.broadcast_array(bigthetao_cube.coord('latitude').points, [1, 2], target_shape)
     pressure = gsw.p_from_z(broadcast_depth, broadcast_latitude)
 
+    absolute_salinity = gsw.SA_from_SP(so_cube.data, pressure, broadcast_longitude, broadcast_latitude)
+    conservative_temperature = bigthetao_cube.data  
+#   conservative_temperature = gsw.CT_from_pt(absolute_salinity, thetao_cube.data)
+
     if args.coefficient == 'alpha':
-        coefficient_data = gsw.alpha(so_cube.data, bigthetao_cube.data, pressure)
+        coefficient_data = gsw.alpha(absolute_salinity, conservative_temperature, pressure)
         var_name = 'alpha'
         standard_name = 'thermal_expansion_coefficient'
         long_name = 'thermal expansion coefficient'
         units = '1/K'
     elif args.coefficient == 'beta':
-        coefficient_data = gsw.beta(so_cube.data, bigthetao_cube.data, pressure)
+        coefficient_data = gsw.beta(absolute_salinity, conservative_temperature, pressure)
         var_name = 'beta'
         standard_name = 'saline_contraction_coefficient'
         long_name = 'saline contraction coefficient'
