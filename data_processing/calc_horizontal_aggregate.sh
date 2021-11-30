8b026014da1e0131ff777a03c0c1a7e5eed09ea1
Damien Irving <irving.damien@gmail.com>
2021-12-01 09:43:07 +1100

    Handle thetao or so

--- data_processing/calc_horizontal_aggregate.sh
+++ data_processing/calc_horizontal_aggregate.sh
@@ -4,7 +4,8 @@
 #             
 
 function usage {
-    echo "USAGE: bash $0 outdir infiles"
+    echo "USAGE: bash $0 var outdir infiles"
+    echo "   var:          Variable (so or thetao)"
     echo "   outdir:       Output directory"
     echo "   infiles:      Input file names"
     exit 1
@@ -12,16 +13,24 @@ function usage {
 
 python=/g/data/e14/dbi599/miniconda3/envs/cmip/bin/python
 script_dir=/home/599/dbi599/ocean-analysis/data_processing
-outdir=$1
+var=$1
+outdir=$2
+shift
 shift
 infiles=($@)
 basin_file=/g/data/e14/dbi599/CMIP6/CMIP/CSIRO-ARCCSS/ACCESS-CM2/historical/r1i1p1f1/Ofx/basin/gn/v20191108/basin_Ofx_ACCESS-CM2_historical_r1i1p1f1_gn.nc
 volume_file=/g/data/fs38/publications/CMIP6/CMIP/CSIRO-ARCCSS/ACCESS-CM2/historical/r1i1p1f1/Ofx/volcello/gn/v20191108/volcello_Ofx_ACCESS-CM2_historical_r1i1p1f1_gn.nc
 
+if [[ "${var}" == "so" ]] ; then
+    long_var=sea_water_salinity
+elif [[ "${var}" == "thetao" ]] ; then
+    long_var=sea_water_potential_temperature
+fi
+
 for infile in "${infiles[@]}"; do
     filename=`echo ${infile} | rev | cut -d / -f 1 | rev`
-    outfile=`echo ${filename} | sed s:so_:so-globe-zonal-mean_:`
-    command="${python} ${script_dir}/calc_horizontal_aggregate.py ${infile} sea_water_salinity zonal mean ${outdir}/${outfile} --basin ${basin_file} globe --weights ${volume_file}"
+    outfile=`echo ${filename} | sed s:${var}_:${var}-globe-zonal-mean_:`
+    command="${python} ${script_dir}/calc_horizontal_aggregate.py ${infile} ${long_var} zonal mean ${outdir}/${outfile} --basin ${basin_file} globe --weights ${volume_file}"
     echo ${command}
     ${command}
     echo ${outdir}/${outfile}
